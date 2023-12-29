7135a2c0162471dab7e2ced84c93ba6b5f267f65
Kim Handong <khdkkhdd@gmail.com>
2023-12-29 11:30:48 +0900

    Title: 연속되는 수 3, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231229/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int prev = -1, cur = -1;
+        int cnt = 1, max = -1;
+        for (int i = 0; i < N; i++) {
+            cur = Integer.parseInt(br.readLine());
+
+            if (i != 0 && prev * cur > 0) cnt++;
+            else cnt = 1;
+
+            max = Math.max(cnt, max);
+            prev = cur;
+        }
+
+        System.out.println(max);
+    }
+}
