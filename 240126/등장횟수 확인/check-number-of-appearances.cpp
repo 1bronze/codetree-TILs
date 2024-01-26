f5e06e5b5bf7e8961fb5fb3ac5be925bef2fab8b
Kim Handong <khdkkhdd@gmail.com>
2024-01-26 18:34:29 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/등장횟수 확인/check-number-of-appearances.cpp
+++ 240126/등장횟수 확인/check-number-of-appearances.cpp
@@ -5,7 +5,7 @@ int main() {
     int cnt = 0;
     for (int i=0; i<5; i++) {
         int tmp; cin >> tmp;
-        if(cnt%2==0) cnt++;
+        if(tmp%2==0) cnt++;
     }
     cout << cnt;
     return 0;
