2bbf311b8be0c91c368966577025763065ecea88
Kim Handong <khdkkhdd@gmail.com>
2024-01-26 18:25:55 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/자연수면 출력/output-only-natural-number.cpp
+++ 240126/자연수면 출력/output-only-natural-number.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main() {
     int a, b; cin >> a >> b;
+
+    if(a<=0) {
+        cout << 0;
+        return 0;
+    }
+
     for(int i=0; i<b; i++) cout << a;
     return 0;
 }
