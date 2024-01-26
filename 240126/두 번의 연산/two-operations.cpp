362e571e7e8e77fad129608fb2ca6d447a5655cb
Kim Handong <khdkkhdd@gmail.com>
2024-01-26 18:01:42 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240126/두 번의 연산/two-operations.cpp
+++ 240126/두 번의 연산/two-operations.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     int a; cin >> a;
-    if (a%2) a+=3;
-    if (!a%3) a/=3;
+    if (a%2 == 1) a+=3;
+    if (a%3 == 0) a/=3;
     cout << a;
     return 0;
 }
