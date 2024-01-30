79d60dc2658b84aaee29ff3ccb2688d2d1b31635
Kim Handong <khdkkhdd@gmail.com>
2024-01-30 19:48:15 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// 65~90
+
+int main() {
+    char cur = 65;
+    int n; cin >> n;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i>j) cout << "  ";
+            else cout << cur++ << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
