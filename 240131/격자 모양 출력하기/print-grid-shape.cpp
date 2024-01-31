31d1674ee74a15fc79dd49e15388bebf2a48f495
Kim Handong <khdkkhdd@gmail.com>
2024-01-31 15:30:45 +0900

    Title: 격자 모양 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int arr[11][11];
+
+int main() {
+    int  n, m;
+    cin >> n >> m;
+
+    for (int i=1; i<=n; i++) {
+        int y, x; cin >> y >> x;
+        arr[y][x] = y * x;
+    }
+
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
