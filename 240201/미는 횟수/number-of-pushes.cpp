b98c61e768b3da54027fceeb9c04b82c315ebbc3
Dong Kim <khdkkhdd@gmail.com>
2024-02-01 13:07:13 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240201/미는 횟수/number-of-pushes.cpp
+++ 240201/미는 횟수/number-of-pushes.cpp
@@ -5,16 +5,17 @@ int main() {
     string target, cmp;
     cin >> target >> cmp;
 
-    int cnt = 0;
+    int cnt = 1;
     bool flag = false;
     while(target.length() == cmp.length() && cnt <= target.length()) {
         cmp = cmp[cmp.length()-1] + cmp.substr(0, cmp.length()-1);
-        cnt++;
 
         if(target == cmp) {
             flag = true;
             break;
         }
+
+        cnt++;
     }
 
     cout << ((flag) ? cnt : '-1'); 
