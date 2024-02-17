8f68df1aeb288a2ae5b36155d4c5c6fd2dd41e3c
Dong Kim <khdkkhdd@gmail.com>
2024-02-17 10:35:45 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1486ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240217/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+dx = [1, 0]
+dy = [0, 1]
+
+lst = [list(map(int, input().split())) for i in range(n)]
+
+deq = deque()
+deq.append((0, 0))
+while deq:
+    x, y = deq.popleft()
+    lst[x][y] = 2
+
+    for i in range(2):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
+        
+        if lst[nx][ny] != 1:
+            continue
+        
+        deq.append((nx, ny))
+
+if lst[n-1][m-1] == 2:
+    print(1)
+else:
+    print(0)
