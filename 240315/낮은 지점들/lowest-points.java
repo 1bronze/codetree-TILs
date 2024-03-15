4e297c0ccb37ee0de1dcf457140b3f55ba91d0b6
Dong Kim <khdkkhdd@gmail.com>
2024-03-15 10:36:42 +0900

    Title: 낮은 지점들, Time: 1518ms, Memory: 59MB, Status: Passed - CodeTree

---
+++ 240315/낮은 지점들/lowest-points.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Long, Long> map = new HashMap<>();
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+
+            if (map.containsKey(x)) {
+                if(map.get(x) > y) {
+                    map.put(x, y);
+                }
+            } else {
+                map.put(x,y);
+            }
+        }
+
+        long ans = 0;
+
+        List<Long> values = map.values().stream().collect(Collectors.toList());
+
+        for(Long i : values){
+            ans+= i;
+        }
+
+        System.out.println(ans);
+
+    }
+}
