459b22a96308854b8431e55865e360d84e89a9f5
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 16:35:25 +0900

    Title: 007, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 240319/007/007.js
+++ 240319/007/007.js
@@ -1,21 +1,16 @@
-class Spy {
-    constructor(secretCode, meetingPoint, time) {
-        this.secretCode = secretCode;
-        this.meetingPoint = meetingPoint;
-        this.time = time;
-    }
-}
-
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력
-const [sCode, mPoint, time] = input[0].split(' ');
+const [s_code, m_point, given_time] = input[0].split(' ');
 
-// 객체 생성
-const s = new Spy(sCode, mPoint, parseInt(time));
+// 객체 생성 대신, 배열을 사용합니다.
+const s = [s_code, m_point, parseInt(given_time)];
+
+// 배열 원소들을 각 변수에 대입
+const [secret_code, meeting_point, time] = s;
 
 // 출력
-console.log(`secret code : ${s.secretCode}`);
-console.log(`meeting point : ${s.meetingPoint}`);
-console.log(`time : ${s.time}`);
+console.log(`secret code : ${secret_code}`);
+console.log(`meeting point : ${meeting_point}`);
+console.log(`time : ${time}`);
