f0f99b7ee28190fec50abf54624b21ee82297d89
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 17:26:02 +0900

    Title: 줄 세우기, Time: 323ms, Memory: 18MB, Status: Passed - CodeTree

--- 240319/줄 세우기/line-up-students.js
+++ 240319/줄 세우기/line-up-students.js
@@ -1,30 +1,22 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-class Student {
-    constructor(height, weight, number) {
-        this.height = height;
-        this.weight = weight;
-        this.number = number;
-    }
-}
-
 // 변수 선언 및 입력
 const n = parseInt(input.shift()); // 첫 번째 입력값을 n으로 할당 후 배열에서 제거
 let students = [];
 for (let i = 0; i < n; i++) {
     const [height, weight] = input[i].split(' ').map(Number);
-    students.push(new Student(height, weight, i + 1));
+    students.push([height, weight, i + 1]);
 }
 
 // Custom Comparator를 활용한 정렬
 students.sort((a, b) => {
-    if (a.height !== b.height) return b.height - a.height;
-    if (a.weight !== b.weight) return b.weight - a.weight;
-    return a.number - b.number;
+    if (a[0] !== b[0]) return b[0] - a[0];
+    if (a[1] !== b[1]) return b[1] - a[1];
+    return a[2] - b[2];
 });
 
 // 출력
 for (let i = 0; i < students.length; i++) {
-    console.log(`${students[i].height} ${students[i].weight} ${students[i].number}`);
+    console.log(`${students[i][0]} ${students[i][1]} ${students[i][2]}`);
 }
