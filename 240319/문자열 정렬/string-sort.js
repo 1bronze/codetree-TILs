1f28292f0cf62739778f3d2cce092108a8e4bb98
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 15:07:50 +0900

    Title: 문자열 정렬, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240319/문자열 정렬/string-sort.js
@@ -0,0 +1,11 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+const s = fs.readFileSync(0).toString().split('');
+
+// 각 문자를 원소로 담는 리스트 만들기
+const sortedList = s.sort();
+// 각 문자를 원소로 갖는 리스트를 하나의 문자열로 합치기
+const sortedStr = sortedList.join('');
+
+// 출력
+console.log(sortedStr);
