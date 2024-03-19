04d3c4a3a9cb9f4f3f82ed07ddfd3af347f996ea
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 19:16:33 +0900

    Title: 오름 내림차순 정렬, Time: 205ms, Memory: 47MB, Status: Passed - CodeTree

--- 240319/오름 내림차순 정렬/inc-dec-sorting.js
+++ 240319/오름 내림차순 정렬/inc-dec-sorting.js
@@ -1,12 +1,22 @@
-// 변수 선언 및 입력
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const nums = input[1].split(' ').map(Number);
+
+// 변수 선언 및 입력
+const n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
 
 // 오름차순 정렬
-nums.sort((a, b) => a - b);
-console.log(nums.join(' '));
+arr.sort((a, b) => a - b);
+
+for (let elem of arr) {
+    process.stdout.write(`${elem} `);
+}
+console.log();
 
 // 내림차순 정렬
-nums.sort((a, b) => b - a);
-console.log(nums.join(' '));
+arr.sort((a, b) => b - a);
+
+for (let elem of arr) {
+    process.stdout.write(`${elem} `);
+}
+console.log();
