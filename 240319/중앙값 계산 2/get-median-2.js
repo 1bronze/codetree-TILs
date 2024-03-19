108a00a56ad8784caca52e9591a9a40201a9d82d
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 16:31:19 +0900

    Title: 중앙값 계산 2, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

--- 240319/중앙값 계산 2/get-median-2.js
+++ 240319/중앙값 계산 2/get-median-2.js
@@ -3,14 +3,17 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력:
 const n = parseInt(input[0]);
-const arr = input[1].split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
 
 // 홀수 번째 수를 지날때마다 정렬을 진행한 후 가운데 값을 출력합니다.
+let answer = "";
 for (let i = 0; i < n; i++) {
     if (i % 2 === 0) {
         // 오름차순 정렬을 진행합니다.
         const sortedArr = arr.slice(0, i + 1).sort((a, b) => a - b);
         // 가운데 값을 출력합니다.
-        console.log(sortedArr[Math.floor(i / 2)], "");
+        answer += sortedArr[Math.floor(i / 2)] + ' ';
     }
 }
+
+console.log(answer);
