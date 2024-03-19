0087e85401273c9e252020fdf127bad0145a0348
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 17:42:36 +0900

    Title: 원점으로부터의 거리, Time: 214ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240319/원점으로부터의 거리/distance-from-origin.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const n = parseInt(input.shift()); // 첫 번째 입력값을 n으로 할당 후 배열에서 제거
+let distances = [];
+
+// 원점과의 거리를 계산하는 함수입니다.
+function getDistFromOrigin(x, y) {
+    return Math.abs(x) + Math.abs(y);
+}
+
+for (let i = 0; i < n; i++) {
+    const [x, y] = input[i].split(' ').map(Number);
+    // 원점과의 거리와 index를 저장합니다.
+    distances.push([getDistFromOrigin(x, y), i + 1]);
+}
+
+distances.sort((a, b) => a[0] - b[0]);
+
+for (let i = 0; i < n; i++) {
+    console.log(distances[i][1]);
+}
