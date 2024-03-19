812c1404422a408af5f44c9c092398141467ab2b
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 15:42:14 +0900

    Title: Top K 숫자 구하기, Time: 194ms, Memory: 47MB, Status: Passed - CodeTree

--- 240319/Top K 숫자 구하기/kth-number.js
+++ 240319/Top K 숫자 구하기/kth-number.js
@@ -1,11 +1,12 @@
-// 변수 선언  및 입력
+// 변수 선언 및 입력
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
-const k = Number(input[0].split(' ')[1]);
-const nums = input[1].trim().split(' ');
+const n = input[0].split(' ')[0];
+const k = input[0].split(' ')[1];
+const nums = input[1].trim().split(' ').map(Number);
 
 // nums를 정렬합니다.
-nums.sort();
+nums.sort((a, b) => a - b);
 
 // k번째 원소를 출력합니다.
 console.log(nums[k - 1]);
