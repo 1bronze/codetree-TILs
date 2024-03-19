56f5249d67785a6409d3937447b07ff45583776b
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 16:21:18 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 229ms, Memory: 46MB, Status: Passed - CodeTree

--- 240319/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
+++ 240319/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.js
@@ -1,9 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const ASCCI_NUM = 128;
+
 const str1 = input[0];
 const str2 = input[1];
 
-// 각 문자열을 정렬했을 때 두 문자열이 일치하는지 비교합니다.
-const sortedStr1 = str1.split('').sort().join('');
-const sortedStr2 = str2.split('').sort().join('');
-console.log(sortedStr1 === sortedStr2 ? "Yes" : "No");
+// 0으로 초기화 된 count 배열을 정의합니다.
+let count = new Array(ASCCI_NUM).fill(0);
+
+// 첫 번째 문자열을 순회하며 각 문자의 개수를 세줍니다.
+for (let char1 of str1) {
+    count[char1.charCodeAt(0)] += 1;
+}
+
+// 두 번째 문자열을 순회하며 각 문자의 개수를 세줍니다.
+for (let char2 of str2) {
+    count[char2.charCodeAt(0)] -= 1;
+}
+
+// count 배열을 순회하면서 모든 문자의 개수가 동일한지를 확인합니다.
+for (let i = 0; i < ASCCI_NUM; i++) {
+    if (count[i] !== 0) {
+        console.log("No");
+        process.exit(0);
+    }
+}
+
+console.log("Yes");
