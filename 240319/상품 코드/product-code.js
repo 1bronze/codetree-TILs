4361682afa732b7da0c0740797fc625bb957d05e
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 16:56:59 +0900

    Title: 상품 코드, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240319/상품 코드/product-code.js
@@ -0,0 +1,22 @@
+class Product {
+    constructor(prod_id, code) {
+        this.prod_id = prod_id;
+        this.code = code;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 첫 번째 상품 생성
+const product1 = new Product("codetree", 50);
+
+// 변수 선언 및 입력
+const [prod2_id, code2] = input[0].split(' ');
+
+// 두 번째 상품 생성
+const product2 = new Product(prod2_id, parseInt(code2));
+
+// 출력
+console.log(`product ${product1.code} is ${product1.prod_id}`);
+console.log(`product ${product2.code} is ${product2.prod_id}`);
