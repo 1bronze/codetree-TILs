c55cec5616a4fed631d4d60fe70dcd42e7a22e8c
Dong Kim <khdkkhdd@gmail.com>
2024-03-19 16:55:11 +0900

    Title: 폭탄 해제, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240319/폭탄 해제/bomb-removal.js
@@ -0,0 +1,21 @@
+class Bomb {
+    constructor(unlock_code, linear_color, time) {
+        this.unlock_code = unlock_code;
+        this.linear_color = linear_color;
+        this.time = time;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const [u_code, l_color, time] = input[0].split(' ');
+
+// 객체 생성
+const b = new Bomb(u_code, l_color, parseInt(time));
+
+// 출력
+console.log(`code : ${b.unlock_code}`);
+console.log(`color : ${b.linear_color}`);
+console.log(`second : ${b.time}`);
