eddb14c89f3f53d0fdb3dbab6388c01b3ec1c384
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 11:32:40 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240320/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.js
@@ -0,0 +1,29 @@
+class Student {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const students = [];
+for (let i = 1; i <= n; i++) {
+    const [name, height, weight] = input[i].split(' ');
+    students.push(new Student(name, Number(height), Number(weight)));
+}
+
+// Custom Comparator를 활용한 정렬
+students.sort((a, b) => {
+    if (a.height === b.height) return b.weight - a.weight;
+    return a.height - b.height;
+});
+
+// 출력
+for (let student of students) {
+    console.log(student.name, student.height, student.weight);    
+}
