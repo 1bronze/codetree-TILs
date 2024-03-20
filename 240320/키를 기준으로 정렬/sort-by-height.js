76cd65972daa05bf1f46c3c6621d0a7f140dbb21
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 11:08:47 +0900

    Title: 키를 기준으로 정렬, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/키를 기준으로 정렬/sort-by-height.js
+++ 240320/키를 기준으로 정렬/sort-by-height.js
@@ -1,18 +1,26 @@
+class Student {
+  constructor(name, height, weight) {
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+  }
+}
+
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력
-const n = Number(input.shift());
+const n = Number(input[0]);
 const students = [];
-for (let i = 0; i < n; i++) {
-  let [name, height, weight] = input[i].split(' ');
-  students.push([name, Number(height), Number(weight)]);
+for (let i = 1; i <= n; i++) {
+  const [name, height, weight] = input[i].split(' ');
+  students.push(new Student(name, Number(height), Number(weight)));
 }
 
 // Custom Comparator를 활용한 정렬
-students.sort((a, b) => a[1] - b[1]);
+students.sort((a, b) => a.height - b.height);
 
 // 출력
 for (let student of students) {
-    console.log(`${student[0]} ${student[1]} ${student[2]}`);
+  console.log(`${student.name} ${student.height} ${student.weight}`);
 }
