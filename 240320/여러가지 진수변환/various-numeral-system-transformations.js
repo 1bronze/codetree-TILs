c0dfaea130dd74e12b1107cc5f613764b58d5f33
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 13:08:24 +0900

    Title: 여러가지 진수변환, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240320/여러가지 진수변환/various-numeral-system-transformations.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const [nInit, b] = input[0].split(' ').map(Number);
+let n = nInit; // n을 변화시키면서 사용하기 위해 복사해 사용
+const digits = [];
+
+// b진수로 변환합니다.
+while (true) {
+    if (n < b) {
+        digits.push(n);
+        break;
+    }
+
+    digits.push(n % b);
+    n = Math.floor(n / b); // JS에서는 나눗셈 결과가 자동으로 float이 되므로 floor함수로 내림해야 합니다.
+}
+
+// b진수를 출력 합니다.
+// 뒤집은 다음에 출력해야 함에 유의합니다.
+let result = '';
+for (let i = digits.length - 1; i >= 0; i--) {
+    result += digits[i];
+}
+console.log(result);
