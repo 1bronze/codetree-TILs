9366e9b0d90658055020903a4cc6a69a4fa7b84a
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 10:42:26 +0900

    Title: 007, Time: 169ms, Memory: 47MB, Status: Wrong Answer - CodeTree

--- 240320/007/007.js
+++ 240320/007/007.js
@@ -1,21 +1,11 @@
-class Spy {
-    constructor(secret_code, meeting_point, time) {
-        this.secret_code = secret_code;
-        this.meeting_point = meeting_point;
-        this.time = time;
-    }
-}
-
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력
-const [s_code, m_point, time] = input[0].split(' ');
+const [s_code, m_point, given_time] = input[0].split(' ');
 
-// 객체 생성
-const s = new Spy(s_code, m_point, parseInt(time));
+// tuple 생성
+const s = [s_code, m_point, Number(given_time)];
 
-// 출력
-console.log(`secret code : ${s.secret_code}`);
-console.log(`meeting point : ${s.meeting_point}`);
-console.log(`time : ${s.time}`);
+// tuple 원소들을 각 변수에 대입
+const [secret_code, meeting_point, time] = s;
