e1a008f32a8b49a3fc7df3a4d1fb56a1e49734fe
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 16:19:40 +0900

    Title: 문자에 따른 명령 2, Time: 246ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 240320/문자에 따른 명령 2/text-based-commands2.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const dirs = input[0];
+let x = 0, y = 0;
+let currDir = 3;
+
+// 동, 남, 서, 북 순으로 dxs, dys를 정의합니다.
+const dxs = [1,  0, -1, 0];
+const dys = [0, -1,  0, 1];
+
+// 움직이는 것을 진행합니다.
+for (let cDir of dirs) {
+    // 반시계방향 90' 회전
+    if (cDir === 'L') {
+        currDir = (currDir - 1 + 4) % 4;
+    }
+    // 시계방향 90' 회전
+    else if (cDir === 'R') {
+        currDir = (currDir + 1) % 4;
+    }
+    // 직진
+    else {
+        x = x + dxs[currDir];
+        y = y + dys[currDir];
+    }
+}
+
+console.log(`${x} ${y}`);
