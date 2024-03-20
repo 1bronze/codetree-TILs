e06ed65d037a89932387fb5d90d52da3cf8b9c66
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 13:54:26 +0900

    Title: 블럭쌓는 명령2, Time: 198ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240320/블럭쌓는 명령2/block-stacking-commands2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const [n, k] = input[0].split(' ').map(Number);
+const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
+
+const blocks = Array(n + 1).fill(0);
+
+// 블럭을 특정 구간에 쌓아줍니다.
+for (let j = 0; j < segments.length; j++) {
+    const [a, b] = segments[j];
+    for (let i = a; i <= b; i++) {
+        blocks[i] += 1;
+    }
+}
+
+// 최댓값을 구합니다.
+const maxNum = Math.max(...blocks);
+console.log(maxNum);
