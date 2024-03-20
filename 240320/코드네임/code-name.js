b9fcc8b1dc82aaecb57734e7660b4a3a41366a50
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 10:51:49 +0900

    Title: 코드네임, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240320/코드네임/code-name.js
+++ 240320/코드네임/code-name.js
@@ -1,10 +1,3 @@
-class User {
-    constructor(code_name, score) {
-        this.code_name = code_name;
-        this.score = score;
-    }
-}
-
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -13,17 +6,21 @@ const users = [];
 
 for (let i = 0; i < 5; i++) {
     const [code_name, score] = input[i].split(' ');
-    users.push(new User(code_name, parseInt(score)));
+    users.push([code_name, Number(score)]);
 }
 
 
 // 최소 점수를 갖는 유저 찾기
 let min_idx = 0;
 for (let i = 1; i < 5; i++) {
-    if (users[min_idx].score > users[i].score) {
+    const [, min_score] = users[min_idx];
+    const [, curr_user_score] = users[i];
+    
+    if (min_score > curr_user_score) {
         min_idx = i;
     }
 }
 
 // 출력
-console.log(users[min_idx].code_name, users[min_idx].score);
+const [code_name, score] = users[min_idx];
+console.log(code_name, score);
