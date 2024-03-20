d62bda71c52c379e7d0dfc122f45ff7f7bc5c7c6
Dong Kim <khdkkhdd@gmail.com>
2024-03-20 13:04:26 +0900

    Title: 2진수로 변환하기, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240320/2진수로 변환하기/convert-to-binary.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+let n = Number(input[0]);
+const digits = [];
+
+// 이진수로 변환합니다.
+while (true) {
+    if (n < 2) {
+        digits.push(n);
+        break;
+    }
+
+    digits.push(n % 2);
+    n = Math.floor(n / 2);
+}
+
+// 이진수를 출력 합니다.
+// 뒤집은 다음에 출력해야 함에 유의합니다.
+let binaryString = '';
+for (let digit of digits.reverse()) { // 역순 처리는 reverse() 메서드를 사용합니다.
+    binaryString += digit;
+}
+console.log(binaryString);
