42feb4874441ad29c17dc25f39d18579d194664e
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 14:52:10 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 245ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240321/T를 초과하는 연속 부분 수열/subsequence-above-t.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력:
+const [n, t] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// 연속하여 t보다 큰 숫자가 나오는 횟수를 구해보며,
+// 그 중 최댓값을 갱신합니다.
+let ans = 0, cnt = 0;
+arr.forEach(elem => {
+    // Case 1
+    if (elem > t) {
+        cnt += 1;
+    }
+    // Case 2
+    else {
+        cnt = 0;
+    }
+    
+    ans = Math.max(ans, cnt);
+});
+
+console.log(ans);
