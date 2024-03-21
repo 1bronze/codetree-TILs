dc57bd80937ba7bbf07c29d240094a8a1ce3c6bb
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 13:29:57 +0900

    Title: 국영수 순이지, Time: 273ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240321/국영수 순이지/korean-english-math-order.js
@@ -0,0 +1,31 @@
+class Student {
+    constructor(name, korean, english, math) {
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const students = [];
+for (let i = 1; i <= n; i++) {
+    let [name, korean, english, math] = input[i].split(' ');
+    students.push(new Student(name, Number(korean), Number(english), Number(math)));
+}
+
+// Custom Comparator를 활용한 정렬
+students.sort((a, b) => {
+    if (a.korean !== b.korean) return b.korean - a.korean;
+    if (a.english !== b.english) return b.english - a.english;
+    return b.math - a.math;
+});
+
+// 출력
+students.forEach(student => {
+    console.log(`${student.name} ${student.korean} ${student.english} ${student.math}`);
+});
