ddd90fc500a1bfdc633a1b2f747b606cec6c2380
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 19:17:35 +0900

    Title: 밭의 높이를 고르게하기, Time: 265ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240321/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const INT_MAX = Number.MAX_SAFE_INTEGER;
+
+// 변수 선언 및 입력:
+const [n, h, t] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+// 모든 구간의 시작점을 잡아봅니다.
+let minCost = INT_MAX;
+for (let i = 0; i <= n - t; i++) {
+    // 해당 구간을 고르게 할 때의 비용을 구합니다.
+    let cost = 0;
+    for (let j = i; j < i + t; j++) {
+        cost += Math.abs(arr[j] - h);
+    }
+    
+    // 최솟값을 구합니다.
+    minCost = Math.min(minCost, cost);
+}
+
+console.log(minCost);
