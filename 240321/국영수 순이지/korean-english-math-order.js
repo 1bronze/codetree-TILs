d2b45cfad9334515b92b02045b5d7fb426b72bed
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 13:30:27 +0900

    Title: 국영수 순이지, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240321/국영수 순이지/korean-english-math-order.js
+++ 240321/국영수 순이지/korean-english-math-order.js
@@ -1,12 +1,3 @@
-class Student {
-    constructor(name, korean, english, math) {
-        this.name = name;
-        this.korean = korean;
-        this.english = english;
-        this.math = math;
-    }
-}
-
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -15,17 +6,17 @@ const n = Number(input[0]);
 const students = [];
 for (let i = 1; i <= n; i++) {
     let [name, korean, english, math] = input[i].split(' ');
-    students.push(new Student(name, Number(korean), Number(english), Number(math)));
+    students.push([name, Number(korean), Number(english), Number(math)]);
 }
 
 // Custom Comparator를 활용한 정렬
 students.sort((a, b) => {
-    if (a.korean !== b.korean) return b.korean - a.korean;
-    if (a.english !== b.english) return b.english - a.english;
-    return b.math - a.math;
+    if (a[1] !== b[1]) return b[1] - a[1]; // 국어점수 내림차순
+    if (a[2] !== b[2]) return b[2] - a[2]; // 영어점수 내림차순
+    return b[3] - a[3]; // 수학점수 내림차순
 });
 
 // 출력
-students.forEach(student => {
-    console.log(`${student.name} ${student.korean} ${student.english} ${student.math}`);
-});
+for (let student of students) {
+    console.log(`${student[0]} ${student[1]} ${student[2]} ${student[3]}`);
+}
