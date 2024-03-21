319ea9a4d2df69ca8afb6712b62f86254ca231e2
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 14:15:53 +0900

    Title: 신기한 타일 뒤집기, Time: 250ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240321/신기한 타일 뒤집기/strange-flipping-tiles.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const MAX_K = 100000;
+// 변수 선언 및 입력
+let n = Number(input[0]);
+let a = Array(2 * MAX_K + 1).fill(0);
+let b = 0, w = 0;
+
+let cur = MAX_K;
+for (let i = 1; i <= n; i++) {
+    let [x, c] = input[i].split(' ');
+    x = Number(x);
+
+    if (c === 'L') {
+        // x칸 왼쪽으로 칠합니다.
+        while (x > 0) {
+            a[cur] = 1;
+            x -= 1;
+
+            if (x) {
+                cur -= 1;
+            }
+        }
+    } else {
+        // x칸 오른쪽으로 칠합니다.
+        while (x > 0) {
+            a[cur] = 2;
+            x -= 1;
+
+            if (x) {
+                cur += 1;
+            }
+        }
+    }
+}
+
+for (let i = 0; i < 2 * MAX_K + 1; i++) {
+    if (a[i] === 1) {
+        w += 1;
+    }
+    if (a[i] === 2) {
+        b += 1;
+    }
+}
+
+// 정답을 출력합니다.
+console.log(`${w} ${b}`);
