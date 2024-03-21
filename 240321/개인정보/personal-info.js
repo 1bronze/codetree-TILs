015c03cd85f897e5901798df5e717f6054bf6cf4
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 13:36:43 +0900

    Title: 개인정보, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240321/개인정보/personal-info.js
@@ -0,0 +1,37 @@
+class Student {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const students = [];
+for (let i = 0; i < 5; i++) {
+    const [name, height, weight] = input[i].split(' ');
+    students.push(new Student(name, Number(height), Number(weight).toFixed(1)));
+}
+
+// Custom Comparator를 활용한 정렬 (이름순으로 정렬)
+students.sort((a, b) => a.name >= b.name ? 1 : -1);
+
+console.log("name");
+// 이름순으로 정렬한 결과 출력
+students.forEach(student => {
+    console.log(`${student.name} ${student.height} ${student.weight}`);
+});
+
+console.log();
+
+// Custom Comparator를 활용한 정렬 (키순으로 정렬)
+students.sort((a, b) => b.height - a.height);
+
+console.log("height");
+// 키순으로 정렬한 결과 출력
+students.forEach(student => {
+    console.log(`${student.name} ${student.height} ${student.weight}`);
+});
