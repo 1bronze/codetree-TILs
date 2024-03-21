6497912fe15f1d85edd3c153a2948f2a53dffe9a
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 13:42:23 +0900

    Title: 줄 세우기 2, Time: 213ms, Memory: 19MB, Status: Passed - CodeTree

---
+++ 240321/줄 세우기 2/line-up-students-2.js
@@ -0,0 +1,33 @@
+// 학생들의 정보를 나타내는 클래스 선언
+class Student {
+    constructor(height, weight, number) {
+        this.height = height;
+        this.weight = weight;
+        this.number = number;
+    }
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+const students = [];
+for (let i = 1; i <= n; i++) {
+    const [height, weight] = input[i].split(' ').map(Number);
+    students.push(new Student(height, weight, i));
+}
+
+// custom comparator를 활용한 정렬
+// 키 오름차순, 몸무게 내림차순 순으로 정렬합니다.
+students.sort((a, b) => {
+    if (a.height === b.height) {
+        return b.weight - a.weight; // 몸무게 내림차순
+    }
+    return a.height - b.height; // 키 오름차순
+});
+
+// 결과를 출력합니다.
+students.forEach(student => {
+    console.log(student.height, student.weight, student.number);
+});
