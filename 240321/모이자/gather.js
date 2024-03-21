aa5c89d5444f173bec49363b9e8be249f4eb1509
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 17:47:17 +0900

    Title: 모이자, Time: 204ms, Memory: 14MB, Status: Passed - CodeTree

--- 240321/모이자/gather.js
+++ 240321/모이자/gather.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력
-const n = Number(input.shift());
-const arr = input.shift().split(' ').map(Number);
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
 
 const intMax = Number.MAX_SAFE_INTEGER;
 let minDist = intMax;
