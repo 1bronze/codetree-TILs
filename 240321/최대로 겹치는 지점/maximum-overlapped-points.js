ef84e55790efd0be26e7ffd4c3d9130e05bd8474
Dong Kim <khdkkhdd@gmail.com>
2024-03-21 14:05:51 +0900

    Title: 최대로 겹치는 지점, Time: 254ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240321/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const segments = [];
+for (let i = 1; i <= n; i++) {
+    segments.push(input[i].split(' ').map(Number));
+}
+
+const blocks = Array(101).fill(0);
+
+// 블럭을 특정 구간에 쌓아줍니다.
+segments.forEach(segment => {
+    let [a, b] = segment;
+
+    for (let i = a; i <= b; i++) {
+        blocks[i] += 1;
+    }
+});
+
+// 최댓값을 구합니다.
+const maxNum = Math.max(...blocks);
+console.log(maxNum);
