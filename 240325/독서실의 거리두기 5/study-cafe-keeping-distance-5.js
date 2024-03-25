760c212fc9c202f96f955b364541783c153a22b8
Dong Kim <khdkkhdd@gmail.com>
2024-03-25 11:46:22 +0900

    Title: 독서실의 거리두기 5, Time: 210ms, Memory: 14MB, Status: Passed - CodeTree

--- 240325/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
+++ 240325/독서실의 거리두기 5/study-cafe-keeping-distance-5.js
@@ -1,25 +1,37 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 변수 선언 및 입력
-const [n, m] = input[0].split(' ').map(Number);
-const arr = [0].concat(input[1].trim().split(' ').map(Number));
+const n = Number(input[0]);
+const seat = input[1].split('');
 
-let ans = 0;
-// 어느 지점에서 시작할지 전부 시도해 봅니다.
-// 모든 경우의 수에 대해 최대가 되도록 하는 수를 계산합니다.
-for (let i = 1; i <= n; i++) {
-    // 시작점은 i입니다.
-    let sumElement = 0;
-    let cur = i;
-
-    // m번 움직임을 반복합니다.
-    for (let j = 0; j < m; j++) {
-        sumElement += arr[cur];
-        cur = arr[cur];
+function minDist() {
+    let dist = n;
+    // 둘 다 1인 곳에 대해
+    // 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (seat[i] === '1' && seat[j] === '1') {
+                dist = Math.min(dist, j - i);
+            }
+        }
     }
     
-    ans = Math.max(ans, sumElement);
+    return dist;
+}
+
+let ans = 0;
+// 들어갈 위치를 일일이 정해보며
+// 그 상황에서 가장 가까운 사람간의 거리를 구해
+// 가능한 경우 중 최댓값을 계산합니다.
+for (let i = 0; i < n; i++) {
+    if (seat[i] === '0') {
+        // 비어있는 위치에 인원을 배치합니다.
+        seat[i] = '1';
+        // 가장 가까운 사람간의 거리를 구해 최댓값을 갱신해줍니다.
+        ans = Math.max(ans, minDist());
+        // 다시 채워졌던 값을 되돌려줍니다.
+        seat[i] = '0';
+    }
 }
 
 console.log(ans);
