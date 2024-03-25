16b88aa564bd68849d76c1536f012340ff78b80f
Dong Kim <khdkkhdd@gmail.com>
2024-03-25 16:13:12 +0900

    Title: 최대공약수 구하기, Time: 213ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240325/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -0,0 +1,19 @@
+// 변수 선언 및 입력
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+
+let n = Number(input[0]);
+let m = Number(input[1]);
+
+function findGcd(n, m) {
+    let gcd = 0;
+    for (let i = 1; i <= Math.min(n, m); i++) {
+        if (n % i === 0 && m % i === 0) {
+            gcd = i;
+        }
+    }
+
+    process.stdout.write(gcd + "\n");
+}
+
+findGcd(n, m);
