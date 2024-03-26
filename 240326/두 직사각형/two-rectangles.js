2fbfa516923e0214d68f74bef487c4788c3ba3b1
Dong Kim <khdkkhdd@gmail.com>
2024-03-26 15:06:57 +0900

    Title: 두 직사각형, Time: 187ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/두 직사각형/two-rectangles.js
+++ 240326/두 직사각형/two-rectangles.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
-const [firstLine, secondLine] = fs.readFileSync(0).toString().trim().split('\n');
-const [x1, y1, x2, y2] = firstLine.split(' ').map(Number); // 첫 번째 사각형의 좌표 입력 받기
-const [a1, b1, a2, b2] = secondLine.split(' ').map(Number); // 두 번째 사각형의 좌표 입력 받기
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 겹치는지 확인하는 함수
-function overlapping(x1, y1, x2, y2, a1, b1, a2, b2) {
+// 변수 선언 및 입력:
+const [x1, y1, x2, y2] = input[0].split(' ').map(Number);
+const [a1, b1, a2, b2] = input[1].split(' ').map(Number);
+
+const overlapping = (x1, y1, x2, y2, a1, b1, a2, b2) => {
     // 겹치지 않는 경우에 대한 처리를 먼저 진행합니다.
     if (x2 < a1 || a2 < x1 || b2 < y1 || y2 < b1) {
         return false;
