322f8db0b7c2baf706500bd0dd7aa3c91506aabd
Dong Kim <khdkkhdd@gmail.com>
2024-03-26 17:03:57 +0900

    Title: 비둘기와 전기줄, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/비둘기와 전기줄/pigeons-and-electric-cords.js
+++ 240326/비둘기와 전기줄/pigeons-and-electric-cords.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync(0, "utf8").trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const maxNum = 10;
 const undefinedVal = -1;
