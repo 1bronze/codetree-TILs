77488bdae6697e8b2f7836211503dec8b27dac7c
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 11:00:09 +0900

    Title: 연속된 숫자 만들기 2, Time: 217ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
+++ 240327/연속된 숫자 만들기 2/create-consecutive-numbers-2.js
@@ -1,26 +1,28 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const [a, b, c] = input[0].split(" ").map(Number);
+// 변수 선언 및 입력
+const arr = input[0].split(" ").map(Number);
 
-/*
-abc acb
-bac bca
-cab cba
-*/
+// 주어진 값들을 정렬합니다.
+arr.sort();
 
-const left = Math.min(a, b, c);
-const right = Math.max(a, b, c);
-const mid = a + b + c - left - right;
-
-let ans = 3;
-
-if (mid-left === 1 && right-mid === 1) { // Case 1. 이미 일렬로 선 경우
-    ans = 0;
-} else if (mid-left === 2 || right-mid === 2) { // Case 2. 두 명만 인접해 있는 경우
-    ans = 1;
-} else { // Case 3. 모두 떨어져 있는 경우
-    ans = 2;
+// Case 1. 3개의 숫자가 전부 연속한 경우
+// 이 경우에는 이동할 필요가 없으므로
+// 최소 이동 횟수는 0이 됩니다.
+if (arr[0] + 1 === arr[1] && arr[1] + 1 === arr[2]) {
+    console.log(0);
+}
+    
+// Case 2. 2개의 숫자의 차이가 정확히 2가 나는 경우
+// 이 경우에는 남은 숫자를 두 숫자 사이에 바로 넣어주면 되므로
+// 최소 이동 횟수는 1이 됩니다.
+else if (arr[0] + 2 === arr[1] || arr[1] + 2 === arr[2]) {
+    console.log(1);
 }
 
-console.log(ans);
+// Case 3. 그렇지 않은 경우에는 항상 2번에 걸쳐
+// 3개의 숫자를 연속하게 만드는 것이 가능합니다.
+else {
+    console.log(2);
+}
