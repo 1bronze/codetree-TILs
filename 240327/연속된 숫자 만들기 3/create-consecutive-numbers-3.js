922f1fa01606e08cf9447ff7c0a82b81579558c6
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 13:23:48 +0900

    Title: 연속된 숫자 만들기 3, Time: 193ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240327/연속된 숫자 만들기 3/create-consecutive-numbers-3.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const arr = input[0].split(" ").map(Number);
+
+// 주어진 값들을 정렬합니다.
+arr.sort();
+
+console.log(Math.max(arr[1] - arr[0], arr[2] - arr[1]) - 1);
