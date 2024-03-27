cf972808aa439b1e4daf26ac835a12b25fe332a5
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 13:00:43 +0900

    Title: 독서실의 거리두기 3, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 240327/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -1,41 +1,56 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const arr = [0];
+const INT_MAX = Number.MAX_SAFE_INTEGER;
+
+// 변수 선언 및 입력
 const n = Number(input[0]);
+const seats = input[1].split("").map(Number);
 
+// Step1. 최적의 위치 찾기
+// 인접한 쌍들 중 가장 먼 1간의 쌍을 찾습니다.
+let maxDist = 0;
+let [max_i, max_j] = [-1, -1];
 for (let i = 0; i < n; i++) {
-    arr.push(Number(input[1][i]));
-}
+    if (seats[i] === 1) {
+        for (let j = i + 1; j < n; j++) {
+            if (seats[j] === 1) {
+                // 1간의 쌍을 골랐을 때
+                // 두 좌석간의 거리가 지금까지의 최적의 답 보다 더 좋다면
+                // 값을 갱신해줍니다.
+                if (j - i > maxDist) {
+                    maxDist = j - i;
 
-let dist = 0;
-let last = 1;
+                    // 이때, 두 좌석의 위치를 기억합니다.
+                    [max_i, max_j] = [i, j];
+                }
 
-let l = 1;
-let r = 1;
-
-for (let i = 2; i <= n; i++) {
-    if (arr[i] === 1) {
-
-        if (dist < i - last) {
-            dist = i - last;
-            l = last;
-            r = i;
+                // 인접한 쌍을 찾았으므로 빠져나옵니다.
+                break;
+            }
         }
-        last = i;
     }
 }
 
-let tmp = r - l;
-arr[l + Math.floor(tmp/2)] = 1;
+// Step2. 최적의 위치에 1을 놓습니다.
+// 가장 먼 쌍의 위치 가운데에 놓으면 됩니다.
+const mid = max_i + Math.floor(maxDist / 2);
+seats[mid] = 1;
 
-dist = n;
-last = 1;
-for (let i = 2; i <= n; i++) {
-    if (arr[i] === 1) {
-        dist = Math.min(dist, i - last);
-        last = i;
+// Step3. 이제 인접한 쌍들 중 가장 가까운 1간의 쌍을 찾습니다.
+// 이때의 값이 답이 됩니다.
+let ans = INT_MAX;
+for (let i = 0; i < n; i++) {
+    if (seats[i] === 1) {
+        for (let j = i + 1; j < n; j++) {
+            if (seats[j] === 1) {
+                ans = Math.min(ans, j - i);
+
+                // 인접한 쌍을 찾았으므로 빠져나옵니다.
+                break;
+            }
+        }
     }
 }
 
-console.log(dist);
+console.log(ans);
