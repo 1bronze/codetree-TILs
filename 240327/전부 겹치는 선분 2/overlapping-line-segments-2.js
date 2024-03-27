7ee9a180a7caab1251fd3ef2dc38690d83403f77
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 16:46:17 +0900

    Title: 전부 겹치는 선분 2, Time: 330ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240327/전부 겹치는 선분 2/overlapping-line-segments-2.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const INT_MAX = Number.MAX_SAFE_INTEGER;
+
+// 변수 선언 및 입력:
+const n = parseInt(input[0]);
+let x1List = [], x2List = [];
+for (let i = 1; i <= n; i++) {
+    const [x1, x2] = input[i].split(' ').map(Number);
+    x1List.push(x1);
+    x2List.push(x2);
+}
+
+let ans = false;
+   
+// 모든 선분을 한번씩 지어 보고, 모든 상황에 대해
+// 전부 겹치는 지점을 하나라도 만들 수 있는지 판단합니다.
+for (let skip = 0; skip < n; skip++) {
+    let maxX1 = 0, minX2 = INT_MAX;
+    let possible = false;
+
+    for (let i = 0; i < n; i++) {
+        if (i === skip) continue;
+
+        // 시작점 중 가장 뒤에 있는 좌표와 끝점 중 가장 앞에 있는 점의 좌표를 확인합니다.
+        maxX1 = Math.max(maxX1, x1List[i]);
+        minX2 = Math.min(minX2, x2List[i]);
+    }
+
+    // 만약 어느 한 선분이라도 시작점이 다른 선분의 끝점보다 뒤에 온다면
+    // 선분이 전부 겹칠 수 없습니다.
+    if (minX2 >= maxX1) {
+        possible = true;
+    } else {
+        possible = false;
+    }
+
+    // 만약 한 가지 방법이라도 전부 겹치는 지점을 만들 수 있다면,
+    // 하나의 선분을 적절하게 제거했을 때 전부 겹칠수 있다는 것이 되므로 할 수 있게 됩니다.
+    if (possible) {
+        ans = true;
+    }
+}
+
+if (ans) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
