f5fb03b619b075590971a9130f5c7279f72d7775
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 17:52:42 +0900

    Title: 최소 와이파이 수, Time: 332ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/최소 와이파이 수/minimum-number-of-wifi.js
+++ 240327/최소 와이파이 수/minimum-number-of-wifi.js
@@ -5,10 +5,10 @@ const [n, m] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
 let ans = 0;
-for (let i = 1; i < n; i++) {
-    if (arr[i] === 1 && arr[i - 1] === 1) {
-        arr[i] = 0;
-        arr[i - 1] = 0;
+// let range = 0;
+for (let i = 0; i < n; i++) {
+    if (arr[i] === 1) {
+        i += 2 * m;
         ans++;
     }
 }
