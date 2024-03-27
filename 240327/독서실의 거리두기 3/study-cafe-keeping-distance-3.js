13ef321fa54894a166f406b8c3ecd6c93e9252a0
Dong Kim <khdkkhdd@gmail.com>
2024-03-27 16:44:32 +0900

    Title: 독서실의 거리두기 3, Time: 238ms, Memory: 47MB, Status: Passed - CodeTree

--- 240327/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
+++ 240327/독서실의 거리두기 3/study-cafe-keeping-distance-3.js
@@ -10,7 +10,7 @@ const seats = input[1].split("").map(Number);
 // Step1. 최적의 위치 찾기
 // 인접한 쌍들 중 가장 먼 1간의 쌍을 찾습니다.
 let maxDist = 0;
-let [max_i, max_j] = [-1, -1];
+let [maxI, maxJ] = [-1, -1];
 for (let i = 0; i < n; i++) {
     if (seats[i] === 1) {
         for (let j = i + 1; j < n; j++) {
@@ -22,7 +22,7 @@ for (let i = 0; i < n; i++) {
                     maxDist = j - i;
 
                     // 이때, 두 좌석의 위치를 기억합니다.
-                    [max_i, max_j] = [i, j];
+                    [maxI, maxJ] = [i, j];
                 }
 
                 // 인접한 쌍을 찾았으므로 빠져나옵니다.
@@ -34,7 +34,7 @@ for (let i = 0; i < n; i++) {
 
 // Step2. 최적의 위치에 1을 놓습니다.
 // 가장 먼 쌍의 위치 가운데에 놓으면 됩니다.
-const mid = max_i + Math.floor(maxDist / 2);
+const mid = maxI + Math.floor(maxDist / 2);
 seats[mid] = 1;
 
 // Step3. 이제 인접한 쌍들 중 가장 가까운 1간의 쌍을 찾습니다.
