1bf6b4fe147932c6ce06c08f45e93980300084c0
Dong Kim <khdkkhdd@gmail.com>
2024-03-28 13:54:29 +0900

    Title: A, B, C 찾기, Time: 231ms, Memory: 15MB, Status: Passed - CodeTree

--- 240328/A, B, C 찾기/finding-a-b-c.js
+++ 240328/A, B, C 찾기/finding-a-b-c.js
@@ -1,21 +1,19 @@
+// 변수 선언 및 입력
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const arr = input[0].split(" ").map(Number);
+const n = 7;
+const arr = input[0].trim().split(' ').map(Number);
+
+// 오름차순으로 정렬을 진행합니다.
 arr.sort((a, b) => a - b);
 
-const totalSum = Math.max(...arr);
-arr.pop(arr.indexOf(totalSum));
+// 오름차순으로 정렬했을 때,
+// 가장 작은 숫자는 A,
+// 두 번째로 작은 숫자는 항상 B가 됩니다.
+const a = arr[0], b = arr[1];
+// 또한, 가장 큰 숫자는 항상 A + B + C가 되므로
+// C는 끝 숫자 - A - B가 됩니다
+const c = arr[arr.length - 1] - a - b;
 
-for (let i = 0; i < 6; i++) {
-    for (let j = i + 1; j < 6; j++) {
-        for (let k = j + 1; k < 6; k++) {
-            let [a, b, c] = [arr[i], arr[j], arr[k]];
-
-            if (arr.includes(a + b) && arr.includes(b + c) && arr.includes(c + a)) {
-                console.log(arr[i], arr[j], arr[k]);
-                process.exit();
-            }
-        }
-    }
-}
+console.log(a, b, c);
