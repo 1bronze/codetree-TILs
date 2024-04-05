0eb60a2f709627890dc3a231edc57ab7909c5ea9
Dong Kim <khdkkhdd@gmail.com>
2024-04-05 18:04:34 +0900

    Title: 정수 명령 처리 3, Time: 507ms, Memory: 25MB, Status: Passed - CodeTree

--- 240405/정수 명령 처리 3/process-numeric-commands-3.js
+++ 240405/정수 명령 처리 3/process-numeric-commands-3.js
@@ -1,100 +1,66 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }
-}
+const MAX_SIZE = 10000;
 
 class Deque {
     constructor() {  // 빈 덱 하나를 생성합니다.
-        this.count = 0;
-        this.head = null;
-        this.tail = null;
+        this.q = Array(MAX_SIZE).fill(0);
+        this.head = 0;
+        this.tail = 0;
     }
 
     pushFront(item) {  // 덱의 맨 앞에 데이터를 추가합니다.
-        let x = new Node(item);
+        if (this.full()) throw new Error("Deque is full");
 
-        if (this.count === 0) {  // 덱이 비어있다면 head와 tail을 모두 x로 설정합니다.
-            this.head = x;
-            this.tail = x;
-        } else {  // 덱에 기존 값이 있다면 head를 x로 변경합니다.
-            this.head.prev = x;
-            x.next = this.head;
-            this.head = x;
-        }
-        this.count++;  // 덱의 크기를 1 증가시킵니다.
+        this.head = (this.head - 1 + MAX_SIZE) % MAX_SIZE;
+        this.q[this.head] = item;
     }
 
     pushBack(item) {  // 덱의 맨 뒤에 데이터를 추가합니다.
-        let x = new Node(item);
+        if (this.full()) throw new Error("Deque is full");
 
-        if (this.count === 0) {  // 덱이 비어있다면 head와 tail을 모두 x로 설정합니다.
-            this.head = x;
-            this.tail = x;
-        } else {  // 덱에 기존 값이 있다면 tail을 x로 변경합니다.
-            this.tail.next = x;
-            x.prev = this.tail;
-            this.tail = x;
-        }
-        this.count++;  // 덱의 크기를 1 증가시킵니다.
+        this.q[this.tail] = item;
+        this.tail = (this.tail + 1) % MAX_SIZE;
+    }
+
+    full() {  // 덱이 가득 차 있으면 true를 반환합니다.
+        return (this.tail + 1) % MAX_SIZE === this.head;
     }
 
     empty() {  // 덱이 비어있으면 true를 반환합니다.
-        return this.count === 0;
+        return this.head === this.tail;
     }
 
-    size() {  // 덱에 들어있는 데이터 수를 반환합니다.
-        return this.count;
+    size() {  // 덱이 들어있는 데이터 수를 반환합니다.
+        return (this.tail - this.head + MAX_SIZE) % MAX_SIZE;
     }
 
     popFront() {  // 덱의 맨 앞에 있는 데이터를 반환하고 제거합니다.
-        if (this.empty()) {
-            throw new Error("Deque is empty");
-        }
-        let x = this.head;
-        if (this.count === 1) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.head = x.next;
-            this.head.prev = null;
-        }
-        this.count--;
-        return x.value;
+        if (this.empty()) throw new Error("Deque is empty");
+
+        const item = this.q[this.head];
+        this.head = (this.head + 1) % MAX_SIZE;
+        return item;
     }
 
     popBack() {  // 덱의 맨 뒤에 있는 데이터를 반환하고 제거합니다.
-        if (this.empty()) {
-            throw new Error("Deque is empty");
-        }
-        let x = this.tail;
-        if (this.count === 1) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.tail = x.prev;
-            this.tail.next = null;
-        }
-        this.count--;
-        return x.value;
+        if (this.empty()) throw new Error("Deque is empty");
+
+        this.tail = (this.tail - 1 + MAX_SIZE) % MAX_SIZE;
+        return this.q[this.tail];
     }
 
     front() {  // 덱의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
-        if (this.empty()) {
-            throw new Error("Deque is empty");
-        }
-        return this.head.value;
+        if (this.empty()) throw new Error("Deque is empty");
+
+        return this.q[this.head];
     }
 
     back() {  // 덱의 맨 뒤에 있는 데이터를 제거하지 않고 반환합니다.
-        if (this.empty()) {
-            throw new Error("Deque is empty");
-        }
-        return this.tail.value;
+        if (this.empty()) throw new Error("Deque is empty");
+
+        return this.q[(this.tail - 1 + MAX_SIZE) % MAX_SIZE];
     }
 }
+
 
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
