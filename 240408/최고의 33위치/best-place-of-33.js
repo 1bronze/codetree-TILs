c93b7050d543e2dc7502c24a8e36845fbf0dc0ba
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 10:59:55 +0900

    Title: 최고의 33위치, Time: 185ms, Memory: 14MB, Status: Passed - CodeTree

--- 240408/최고의 33위치/best-place-of-33.js
+++ 240408/최고의 33위치/best-place-of-33.js
@@ -1,25 +1,39 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split("\n");
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const n = Number(input[0]);
-const arr = input.slice(1, 1 + n).map(row => row.split(" ").map(Number));
+const grid = input.slice(1).map(line => line.split(' ').map(Number));
 
-const dy = [0, 0, 0, 1, 1, 1, 2, 2, 2];
-const dx = [0, 1, 2, 0, 1, 2, 0, 1, 2];
 
-let ans = 0;
-for (let i = 0; i < n - 2; i++) {
-    for (let j = 0; j < n - 2; j++) {
-        let cnt = 0;
-        for (let k = 0; k < 9; k++) {
-            let ny = j + dy[k];
-            let nx = i + dx[k];
-            if (arr[ny][nx] === 1) {
-                cnt++;
-            }
+// (rowS, colS) ~ (rowE, colE) 사이의 금의 개수를 계산합니다.
+function getNumOfGold(rowS, colS, rowE, colE) {
+    let numOfGold = 0;
+
+    for (let row = rowS; row <= rowE; row++) {
+        for (let col = colS; col <= colE; col++) {
+            numOfGold += grid[row][col];
         }
-        ans = Math.max(ans, cnt);
+    }
+
+    return numOfGold;
+}
+
+let maxGold = 0;
+
+// (row, col)이 3 * 3 격자의 좌측 상단 모서리인 경우를 전부 탐색합니다.
+for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+        // 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
+        if (row + 2 >= n || col + 2 >= n) {
+            continue;
+        }
+        
+        const numOfGold = getNumOfGold(row, col, row + 2, col + 2);
+            
+        // 최대 금의 개수를 저장합니다.
+        maxGold = Math.max(maxGold, numOfGold);
     }
 }
 
-console.log(ans);
+console.log(maxGold);
