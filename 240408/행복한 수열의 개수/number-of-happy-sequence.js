c6a010c99c869ce012d39e2e65868a7c95114c08
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 11:31:53 +0900

    Title: 행복한 수열의 개수, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240408/행복한 수열의 개수/number-of-happy-sequence.js
+++ 240408/행복한 수열의 개수/number-of-happy-sequence.js
@@ -5,19 +5,30 @@ const [n, m] = input[0].split(' ').map(Number);
 const arr = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number));
 
 let ans = 0;
-for (let i = 0; i < n; i++) {
-    let cnt = 0;
-    for (let j = 0; j < n; j++) {
-        if (j - 1 >= 0 && arr[i][j] === arr[i][j - 1]) cnt++;
-        else cnt = 1;
+
+// 각 행마다 연속인 숫자의 개수 세기
+for (let y = 0; y < n; y++) {
+    let cnt = 1; // 초기에는 무조건 1개의 연속 수열을 가짐
+    for (let x = 0; x < n; x++) {
+        if (x == 0) cnt = 1;
+        else if (arr[y][x] === arr[y][x - 1]) cnt++;
+        else {
+            if (cnt >= m) ans++;
+            cnt = 1;
+        }
     }
     if (cnt >= m) ans++;
 }
-for (let i = 0; i < n; i++) {
-    let cnt = 0;
-    for (let j = 0; j < n; j++) {
-        if (j - 1 >= 0 && arr[j][i] === arr[j - 1][i]) cnt++;
-        else cnt = 1;
+
+for (let x = 0; x < n; x++) {
+    let cnt = 1; // 초기에는 무조건 1개의 연속 수열을 가짐
+    for (let y = 0; y < n; y++) {
+        if (y == 0) cnt = 1;
+        else if (arr[y][x] === arr[y - 1][x]) cnt++;
+        else {
+            if (cnt >= m) ans++;
+            cnt = 1;
+        }
     }
     if (cnt >= m) ans++;
 }
