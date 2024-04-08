6d7f87fe3b94ecc2ffec2770e932d328badf8c9f
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 13:10:53 +0900

    Title: 트로미노, Time: 207ms, Memory: 15MB, Status: Passed - CodeTree

--- 240408/트로미노/tromino.js
+++ 240408/트로미노/tromino.js
@@ -1,54 +1,70 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력:
 const [n, m] = input[0].split(' ').map(Number);
 const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
-const dy = [[0, 0, 1, 1], [0, 0, 0], [0, 1, 2]];
-const dx = [[0, 1, 0, 1], [0, 1, 2], [0, 0, 0]];
+// 가능한 모든 모양을 전부 적어줍니다.
+const shapes = [
+    [[1, 1, 0],
+    [1, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 0],
+    [0, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0]],
+
+    [[1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0]],
+
+    [[1, 0, 0],
+    [1, 0, 0],
+    [1, 0, 0]],
+];
+
+// 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+function getMaxSum(x, y) {
+    let maxSum = 0;
+    for (let i = 0; i < 6; i++) {
+        let isPossible = true;
+        let sumOfNums = 0;
+        for (let dx = 0; dx < 3; dx++) {
+            for (let dy = 0; dy < 3; dy++) {
+                if (shapes[i][dx][dy] === 0) {
+                    continue;
+                }
+                if (x + dx >= n || y + dy >= m) {
+                    isPossible = false;
+                } else {
+                    sumOfNums += grid[x + dx][y + dy];
+                }
+            }
+        }
+        
+        if (isPossible) {
+            maxSum = Math.max(maxSum, sumOfNums);
+        }
+    }
+
+    return maxSum;
+}
 
 let ans = 0;
-for (let y = 0; y < n; y++) {
-    for (let x = 0; x < m; x++) {
-        // 첫 번째 모양의 블록
-        let sum = 0; let flag = true;
-        for (let i = 0; i < 4; i++) {
-            let ny = y + dy[0][i];
-            let nx = x + dx[0][i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
-                flag = false;
-                continue;
-            }
-            sum += grid[ny][nx];
-        }
-        for (let i = 0; i < 4; i++) {
-            if (!flag) break;
-            let ny = y + dy[0][i];
-            let nx = x + dx[0][i];
-            sum -= grid[ny][nx];
-            ans = Math.max(ans, sum);
-            sum += grid[ny][nx];
-        }
 
-        // 두 번째 모양의 블록 (가로)
-        sum = 0;
-        for (let i = 0; i < 3; i++) {
-            let ny = y + dy[1][i];
-            let nx = x + dx[1][i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            sum += grid[ny][nx];
-        }
-        ans = Math.max(ans, sum);
-
-        // 두 번째 모양의 블록 (세로)
-        sum = 0;
-        for (let i = 0; i < 3; i++) {
-            let ny = y + dy[2][i];
-            let nx = x + dx[2][i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            sum += grid[ny][nx];
-        }
-        ans = Math.max(ans, sum);
+// 격자의 각 위치에 대하여 탐색하여줍니다.
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+        ans = Math.max(ans, getMaxSum(i, j));
     }
 }
 
