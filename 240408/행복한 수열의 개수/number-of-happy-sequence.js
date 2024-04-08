17a7e4a7ca43e8177bda4db0f7a499da4af5f341
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 12:40:57 +0900

    Title: 행복한 수열의 개수, Time: 245ms, Memory: 14MB, Status: Passed - CodeTree

--- 240408/행복한 수열의 개수/number-of-happy-sequence.js
+++ 240408/행복한 수열의 개수/number-of-happy-sequence.js
@@ -1,30 +1,50 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력:
 const [n, m] = input[0].split(' ').map(Number);
-const arr = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number));
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+let seq = Array(n).fill(0);
 
-let ans = 0;
-
-// 각 행마다 연속인 숫자의 개수 세기
-for (let y = 0; y < n; y++) {
-    let cnt = 1; let tmp = 1;
-    for (let x = 1; x < n; x++) {
-        if (arr[y][x] === arr[y][x - 1]) tmp++;
-        else tmp = 1;
-        cnt = Math.max(cnt, tmp);
+function isHappySequence() {
+    // 주어진 seq가 행복한 수열인지 판단하는 함수입니다.
+    let consecutiveCount = 1, maxCcnt = 1;
+    for (let i = 1; i < n; i++) {
+        if (seq[i - 1] === seq[i]) {
+            consecutiveCount += 1;
+        } else {
+            consecutiveCount = 1;
+        }
+        
+        maxCcnt = Math.max(maxCcnt, consecutiveCount);
     }
-    if (cnt >= m) ans++;
+    
+    // 최대로 연속한 회수가 m이상이면 true를 반환합니다.
+    return maxCcnt >= m;
 }
 
-for (let x = 0; x < n; x++) {
-    let cnt = 1; let tmp = 1;
-    for (let y = 1; y < n; y++) {
-        if (arr[y][x] === arr[y - 1][x]) tmp++;
-        else tmp = 1;
-        cnt = Math.max(cnt, tmp);
+
+let numHappy = 0;
+
+// 먼저 가로로 행복한 수열의 수를 셉니다.
+for (let i = 0; i < n; i++) {
+    seq = grid[i].slice();
+
+    if (isHappySequence()) {
+        numHappy += 1;
     }
-    if (cnt >= m) ans++;
 }
 
-console.log(ans);
+// 세로로 행복한 수열의 수를 셉니다.
+for (let j = 0; j < n; j++) {
+    // 세로로 숫자들을 모아 새로운 수열을 만듭니다.
+    for (let i = 0; i < n; i++) {
+        seq[i] = grid[i][j];
+    }
+
+    if (isHappySequence()) {
+        numHappy += 1;
+    }
+}
+
+console.log(numHappy);
