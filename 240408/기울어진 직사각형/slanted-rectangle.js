c7c0bea0b334cfe06d9b57be0a58b41463545b3c
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 16:29:01 +0900

    Title: 기울어진 직사각형, Time: 261ms, Memory: 27MB, Status: Passed - CodeTree

--- 240408/기울어진 직사각형/slanted-rectangle.js
+++ 240408/기울어진 직사각형/slanted-rectangle.js
@@ -1,61 +1,57 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
 
-const [n, m] = input[0].split(' ').map(Number);
-const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+const MAX_N = 20;
+const DIR_NUM = 4;
 
-// 1번의 길이 설정 + 2번의 길이 설정 > 이중 for문으로 구현
-// 최대 길이는 n-1, 최소 길이는 2
-
-const dy = [0, -1, -1, 1, 1];
-const dx = [0, 1, -1, -1, 1];
-
-function inRange(y, x) {
-    return (y >= 0 && y < n && x >= 0 && x < n);
+let n = Number(input[0]);
+let grid = [];
+for (let i = 1; i <= n; i++) {
+    grid.push(input[i].split(' ').map(Number));
 }
 
-function getSum(y, x, len1, len2) {
-    let curY = y;
-    let curX = x;
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
 
-    let sum = grid[curY][curX];
+function getScore(x, y, k, l) {
+    const dx = [-1, -1, 1, 1];
+    const dy = [1, -1, -1, 1];
+    const moveNum = [k, l, k, l];
+    
+    let sumOfNums = 0;
 
-    for (let l = 1; l < len1; l++) { // 1번 방향
-        curY += dy[1];
-        curX += dx[1];
-        sum += grid[curY][curX];
+    // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for (let d = 0; d < DIR_NUM; d++) {
+        for (let q = 0; q < moveNum[d]; q++) {
+            x += dx[d];
+            y += dy[d];
+                
+            // 기울어진 직사각형이 경계를 벗어나는 경우라면
+            // 불가능하다는 의미로 답이 갱신되지 않도록
+            // 0을 반환합니다.
+            if (!inRange(x, y)) {
+                return 0;
+            }
+			
+            sumOfNums += grid[x][y];
+        }
     }
-    for (let l = 1; l < len2; l++) { // 2번 방향
-        curY += dy[2];
-        curX += dx[2];
-        sum += grid[curY][curX];
-    }
-    for (let l = 1; l < len1; l++) { // 3번 방향
-        curY += dy[3];
-        curX += dx[3];
-        sum += grid[curY][curX];
-    }
-    for (let l = 2; l < len2; l++) { // 4번 방향
-        curY += dy[4];
-        curX += dx[4];
-        sum += grid[curY][curX];
-    }
-
-    return sum;
+    
+    return sumOfNums;
 }
 
 let ans = 0;
-for (let y = 1; y < n; y++) {
-    for (let x = 1; x < n; x++) {
-        for (let len1 = 2; len1 < n; len1++) {
-            for (let len2 = 2; len2 < n; len2++) {
-                let [y1, x1] = [y, x];
-                let [y2, x2] = [y + (dy[1] * (len1 - 1)), x + (dx[1] * (len1 - 1))];
-                let [y3, x3] = [y2 + (dy[2] * (len2 - 1)), x2 + (dx[2] * (len2 - 1))];
-                let [y4, x4] = [y3 + (dy[3] * (len1 - 1)), x3 + (dx[3] * (len1 - 1))]
 
-                if (!inRange(y1, x1) || !inRange(y2, x2) || !inRange(y3, x3) || !inRange(y4, x4)) continue;
-                ans = Math.max(ans, getSum(y, x, len1, len2));
+// (i, j)를 시작으로 1, 2, 3, 4 방향
+// 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+// 기울어진 직사각형을 잡아보는
+// 완전탐색을 진행해봅니다.
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        for (let k = 1; k < n; k++) {
+            for (let l = 1; l < n; l++) {
+                ans = Math.max(ans, getScore(i, j, k, l));
             }
         }
     }
