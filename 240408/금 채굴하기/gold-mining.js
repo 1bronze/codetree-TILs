010e73323ec02ee5fa97db4b4c582731ea164f68
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 15:44:06 +0900

    Title: 금 채굴하기, Time: 326ms, Memory: 15MB, Status: Passed - CodeTree

--- 240408/금 채굴하기/gold-mining.js
+++ 240408/금 채굴하기/gold-mining.js
@@ -10,16 +10,32 @@ function getArea(k) {
     return k * k + (k + 1) * (k + 1);
 }
 
+// 주어진 좌표가 격자에 포함되는지 여부를 반환합니다.
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
 // 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
 function getNumOfGold(row, col, k) {
     let numOfGold = 0;
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            if (Math.abs(row - i) + Math.abs(col - j) <= k) {
-                numOfGold += grid[i][j];
+    const dx = [1, 1, -1, -1], dy = [-1, 1, 1, -1];
+    
+    numOfGold += grid[row][col]; // k = 0 일 때 처리
+    
+    for (let currK = 1; currK <= k; currK++) {
+        let currX = row - currK, currY = col; // 순회 시작점 설정
+        for (let dir = 0; dir < 4; dir++) {
+            for (let step = 0; step < currK; step++) {
+                if (inRange(currX, currY)) {
+                    numOfGold += grid[currX][currY];
+                }
+                
+                currX += dx[dir];
+                currY += dy[dir];
             }
         }
     }
+    
     return numOfGold;
 }
 
