f38943f46d2355cb15e1c3dabdf33a033b1b58b3
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 11:11:51 +0900

    Title: 행복한 수열의 개수, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240408/행복한 수열의 개수/number-of-happy-sequence.js
+++ 240408/행복한 수열의 개수/number-of-happy-sequence.js
@@ -7,16 +7,16 @@ const arr = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 let ans = 0;
 for (let i = 0; i < n; i++) {
     let cnt = 1;
-    for (let j = 1; j < n; j++) {
-        if (arr[i][j] === arr[i][j - 1]) cnt++;
+    for (let j = 0; j < n; j++) {
+        if (j - 1 >= 0 && arr[i][j] === arr[i][j - 1]) cnt++;
         else cnt = 1;
     }
     if (cnt >= m) ans++;
 }
 for (let i = 0; i < n; i++) {
     let cnt = 1;
-    for (let j = 1; j < n; j++) {
-        if (arr[j][i] === arr[j - 1][i]) cnt++;
+    for (let j = 0; j < n; j++) {
+        if (j - 1 >= 0 && arr[j][i] === arr[j - 1][i]) cnt++;
         else cnt = 1;
     }
     if (cnt >= m) ans++;
