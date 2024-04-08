88c11255b24dcaa97e23aba4bfb56b566193aa17
Dong Kim <khdkkhdd@gmail.com>
2024-04-08 12:34:08 +0900

    Title: 행복한 수열의 개수, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240408/행복한 수열의 개수/number-of-happy-sequence.js
+++ 240408/행복한 수열의 개수/number-of-happy-sequence.js
@@ -8,26 +8,24 @@ let ans = 0;
 
 // 각 행마다 연속인 숫자의 개수 세기
 for (let y = 0; y < n; y++) {
-    let cnt = 1; // 초기에는 무조건 1개의 연속 수열을 가짐
-    for (let x = 0; x < n; x++) {
-        if (x == 0) cnt = 1;
-        else if (arr[y][x] === arr[y][x - 1]) cnt++;
+    let cnt = 1; let tmp = 1;
+    for (let x = 1; x < n; x++) {
+        if (arr[y][x] === arr[y][x - 1]) tmp++;
         else {
-            if (cnt >= m) ans++;
-            cnt = 1;
+            cnt = Math.max(cnt, tmp);
+            tmp = 1;
         }
     }
     if (cnt >= m) ans++;
 }
 
 for (let x = 0; x < n; x++) {
-    let cnt = 1; // 초기에는 무조건 1개의 연속 수열을 가짐
-    for (let y = 0; y < n; y++) {
-        if (y == 0) cnt = 1;
-        else if (arr[y][x] === arr[y - 1][x]) cnt++;
+    let cnt = 1; let tmp = 1;
+    for (let y = 1; y < n; y++) {
+        if (arr[y][x] === arr[y - 1][x]) tmp++;
         else {
-            if (cnt >= m) ans++;
-            cnt = 1;
+            cnt = Math.max(cnt, tmp);
+            tmp = 1;
         }
     }
     if (cnt >= m) ans++;
