29389ad4de51c83a10190f4e8fbd45af42b8885a
Dong Kim <khdkkhdd@gmail.com>
2024-04-09 10:27:31 +0900

    Title: 컨베이어 벨트, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240409/컨베이어 벨트/conveyor-belt.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, t] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number).concat(input[2].split(" ").map(Number));
+
+for (let i = 0; i < t; i++) {
+    let tmp = arr[2 * n - 1];
+
+    for (let j = arr.length; j > 0; j--)
+        arr[j] = arr[j - 1];
+
+    arr[0] = tmp;
+}
+
+console.log(arr.slice(0, n).join(" "));
+console.log(arr.slice(n, 2 * n).join(" "));
