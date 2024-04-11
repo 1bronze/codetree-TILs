93cae02a2188dc584e98eb0c3f382a2c42e0a986
Dong Kim <khdkkhdd@gmail.com>
2024-04-11 14:56:04 +0900

    Title: 1차원 폭발 게임, Time: 270ms, Memory: 14MB, Status: Passed - CodeTree

--- 240411/1차원 폭발 게임/The-1D-bomb-game.js
+++ 240411/1차원 폭발 게임/The-1D-bomb-game.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 입력 처리
 const [n, m] = input[0].split(' ').map(Number);
-let numbers = input.slice(1, n + 1).map(Number);
+const numbers = input.slice(1, 1 + n).map(Number);
 
-// 주어진 시작점에 대하여 부분 수열의 끝 위치를 반환합니다.
+// 주어진 시작점에 대하여
+// 부분 수열의 끝 위치를 반환합니다.
 function getEndIdxOfExplosion(startIdx, currNum) {
     for (let endIdx = startIdx + 1; endIdx < numbers.length; endIdx++) {
         if (numbers[endIdx] !== currNum) {
@@ -17,31 +17,27 @@ function getEndIdxOfExplosion(startIdx, currNum) {
 
 while (true) {
     let didExplode = false;
+    let currIdx = 0;
 
-    for (let currIdx = 0; currIdx < numbers.length; currIdx++) {
-        let number = numbers[currIdx];
-        // 각 위치마다 그 뒤로 폭탄이 m개 이상 있는지 확인합니다.
-        
-        // 이미 터지기로 예정되어있는 폭탄은 패스합니다.
-        if (number === 0) continue;
-        // currIdx로부터 연속하여 같은 숫자를 갖는 폭탄 중 가장 마지막 위치를 찾아 반환합니다.
-        let endIdx = getEndIdxOfExplosion(currIdx, number);
-        
+    while (currIdx < numbers.length) {
+        let endIdx = getEndIdxOfExplosion(currIdx, numbers[currIdx]);
+
         if (endIdx - currIdx + 1 >= m) {
-            // 연속한 숫자의 개수가 m개 이상인 경우 폭탄이 터졌음을 기록해줍니다.
-            // 터져야 할 폭탄들에 대해 터졌다는 의미로 0을 채워줍니다.
-            for (let i = currIdx; i <= endIdx; i++) {
-                numbers[i] = 0;
-            }
+            // 연속한 숫자의 개수가 m개 이상이면
+            // 폭탄이 터질 수 있는 경우 해당 부분 수열을 잘라내고
+            // 폭탄이 터졌음을 기록해줍니다.
+            numbers.splice(currIdx, endIdx - currIdx + 1);
             didExplode = true;
+        } else {
+            // 주어진 시작 원소에 대하여 폭탄이 터질 수 없는 경우
+            // 다음 원소에 대하여 탐색하여 줍니다.
+            currIdx += 1;
         }
     }
-    
-    // 폭탄이 터진 이후의 결과를 계산하여 반영해줍니다.
-    numbers = numbers.filter(number => number > 0);
-    
-    // 더 이상 폭탄이 터지지 않는다면 종료합니다.
-    if (!didExplode) break;
+
+    if (!didExplode) {
+        break;
+    }
 }
 
 console.log(numbers.length);
