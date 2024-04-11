d53997d7248de5be35a6eef4623b0fa6aee99fbe
Dong Kim <khdkkhdd@gmail.com>
2024-04-11 14:57:26 +0900

    Title: 1차원 폭발 게임, Time: 288ms, Memory: 14MB, Status: Passed - CodeTree

--- 240411/1차원 폭발 게임/The-1D-bomb-game.js
+++ 240411/1차원 폭발 게임/The-1D-bomb-game.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 입력 처리
 const [n, m] = input[0].split(' ').map(Number);
-const numbers = input.slice(1, 1 + n).map(Number);
+let numbers = input.slice(1, n + 1).map(Number);
 
-// 주어진 시작점에 대하여
-// 부분 수열의 끝 위치를 반환합니다.
+// 주어진 시작점에 대하여 부분 수열의 끝 위치를 반환합니다.
 function getEndIdxOfExplosion(startIdx, currNum) {
     for (let endIdx = startIdx + 1; endIdx < numbers.length; endIdx++) {
         if (numbers[endIdx] !== currNum) {
@@ -18,10 +18,10 @@ function getEndIdxOfExplosion(startIdx, currNum) {
 while (true) {
     let didExplode = false;
     let currIdx = 0;
-
+    
     while (currIdx < numbers.length) {
         let endIdx = getEndIdxOfExplosion(currIdx, numbers[currIdx]);
-
+        
         if (endIdx - currIdx + 1 >= m) {
             // 연속한 숫자의 개수가 m개 이상이면
             // 폭탄이 터질 수 있는 경우 해당 부분 수열을 잘라내고
@@ -31,13 +31,11 @@ while (true) {
         } else {
             // 주어진 시작 원소에 대하여 폭탄이 터질 수 없는 경우
             // 다음 원소에 대하여 탐색하여 줍니다.
-            currIdx += 1;
+            currIdx = endIdx + 1;
         }
     }
-
-    if (!didExplode) {
-        break;
-    }
+    
+    if (!didExplode) break;
 }
 
 console.log(numbers.length);
