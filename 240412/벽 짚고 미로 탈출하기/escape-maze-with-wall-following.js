5442ca5fbbd0f36dbb2c27372ac0c231745cede9
Dong Kim <khdkkhdd@gmail.com>
2024-04-12 15:08:07 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 269ms, Memory: 17MB, Status: Passed - CodeTree

--- 240412/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.js
+++ 240412/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.js
@@ -1,128 +1,104 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+const DIR_NUM = 4;
+
+// 변수 선언 및 입력
 const n = Number(input[0]);
-let [y, x] = input[1].split(' ').map(n => Number(n) - 1);
-let grid = input.slice(2, 2 + n).map(line => line.trim().split(''));
+let [currX, currY] = input[1].split(' ').map(Number);
+const a = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
 
-const WALL = '#';
-const BLANK = '.';
+// 미로 탈출이 불가능한지 여부를 판단하기 위해
+// 동일한 위치에 동일한 방향으로 진행했던 적이 있는지를
+// 표시해주는 배열입니다.
+const visited = Array.from(Array(n + 1), () => 
+                Array.from(Array(n + 1), () =>
+                Array(DIR_NUM).fill(false)));
+let elapsedTime = 0;
 
-const EAST = 0;
-const SOUTH = 1;
-const WEST = 2;
-const NORTH = 3;
+// 처음에는 우측 방향을 바라보고 시작합니다.
+let currDir = 0;
 
-let dir = EAST;
-let time = 0;
-let status = new Array(n).fill(0).map(() => new Array(n).fill(0).map(() => new Array(4).fill(false)));
-
-function turnRight() {
-    dir = (dir + 1) % 4;
+// 범위가 격자 안에 들어가는지 확인합니다.
+function inRange(x, y) {
+    return 1 <= x && x <= n && 1 <= y && y <= n;
 }
 
-function turnLeft() {
-    dir = (dir + 3) % 4;
+// 해당 위치에 벽이 있으면 이동이 불가합니다.
+function wallExist(x, y) {
+    return inRange(x, y) && a[x][y] === '#';
 }
 
-function getLeftDir() {
-    return (dir + 3) % 4;
-}
-
-function getFrontDir() {
-    return dir;
-}
-
-function getRightDir() {
-    return (dir + 1) % 4;
-}
-
-function getBackDir() {
-    return (dir + 2) % 4;
-}
-
-function inRange(nextY, nextX) {
-    return (nextY >= 0 && nextY < n && nextX >= 0 && nextX < n);
-}
-
-function isBlank(nextDir) {
-
-    if (nextDir === EAST) 
-        return (!inRange(y, x + 1) || grid[y][x + 1] === BLANK);
-    else if (nextDir === SOUTH)
-        return (!inRange(y + 1, x) || grid[y + 1][x] === BLANK);
-    else if (nextDir === WEST)
-        return (!inRange(y, x - 1) || grid[y][x - 1] === BLANK);
-    else
-        return (!inRange(y - 1, x) || grid[y - 1][x] === BLANK);
-}
-
-function go() {
-    if (dir === EAST) x++;
-    else if (dir === SOUTH) y++;
-    else if (dir === WEST) x--;
-    else y--;
-
-    time++;
-}
-
-while (true) {
-    if (status[y][x][dir]) {
+// 조건에 맞춰 움직여봅니다.
+function simulate() {
+    // 현재 위치에 같은 방향으로 진행한 적이 이미 있었는지 확인합니다.
+    // 이미 한 번 겪었던 상황이라면, 탈출이 불가능 하다는 의미이므로 
+    // -1을 출력하고 프로그램을 종료합니다.
+    if (visited[currX][currY][currDir]) {
         console.log(-1);
-        break;
-    } else {
-        status[y][x][dir] = true;
+        process.exit(0);
     }
-
-    if (isBlank(getRightDir())) {
-        turnRight();
-        go();
-    } else if (isBlank(getFrontDir())) {
-        go();
-    } else if (isBlank(getLeftDir())) {
-        turnLeft();
-        go();
-    } else if (isBlank(getBackDir())) {
-        turnLeft();
-        turnLeft();
-        go();
-    } else {
-        console.log(-1);
-        break;
+    
+    // 현재 상황이 다시 반복되는지를 나중에 확인하기 위해
+    // 현재 상황에 해당하는 곳에 visited 값을 True로 설정합니다.
+    visited[currX][currY][currDir] = true;
+    
+    const dxs = [0, 1, 0, -1], dys = [1, 0, -1, 0];
+    
+    const nextX = currX + dxs[currDir], nextY = currY + dys[currDir];
+    
+    // Step1
+    
+    // 바라보고 있는 방향으로 이동하는 것이 불가능한 경우에는
+    // 반 시계 방향으로 90' 방향을 바꿉니다.
+    if (wallExist(nextX, nextY)) {
+        currDir = (currDir - 1 + 4) % 4;
     }
+    // Step2
+    
+    // Case1
+    // 바라보고 있는 방향으로 이동하는 것이 가능한 경우 중
+    // 바로 앞이 격자 밖이라면 탈출합니다.
+    else if (!inRange(nextX, nextY)) {
+        currX = nextX; currY = nextY;
+        elapsedTime += 1;
+    }
+    // Case 2 & Case 3
+    // 바로 앞이 격자 안에서 이동할 수 있는 곳이라면
+    else {
+        // 그 방향으로 이동했다 가정했을 때 바로 오른쪽에 짚을 벽이 있는지 봅니다.
+        const rx = nextX + dxs[(currDir + 1) % 4];
+        const ry = nextY + dys[(currDir + 1) % 4];
+        
+        // Case2
+        // 그대로 이동해도 바로 오른쪽에 짚을 벽이 있다면
+        // 해당 방향으로 한 칸 이동합니다.
+        if (wallExist(rx, ry)) {
+            currX = nextX; currY = nextY;
+            elapsedTime += 1;
+        }
+        // Case3
+        // 그렇지 않다면 2칸 이동후 방향을 시계방향으로 90' 방향을 바꿉니다.
+        else {
+            currX = rx; currY = ry;
+            currDir = (currDir + 1) % 4;
+            elapsedTime += 2;
+        }
+    }
+}
 
-    if (!inRange(y, x)) {
-        console.log(time);
-        break;
+let index = 2;
+for (let i = 1; i <= n; i++) {
+    const givenRow = input[index++];
+    for (let j = 1; j <= givenRow.length; j++) {
+        a[i][j] = givenRow[j - 1];
     }
 }
 
-/*
-while(1) {
-    if (오른쪽이 뚫려있다) {
-        시계방향으로 90' 회전한다.
-        한칸 앞으로 간다.
-        시간을 +1한다.
-    } else if (앞쪽이 뚫려있다) {
-        한칸 앞으로 간다.
-        시간을 +1한다.
-    } else if (왼쪽이 뚫려있다) {
-        반시계방향으로 90' 회전한다.
-        한칸 앞으로 간다.
-        시간을 +1한다.
-    } else if (뒷쪽이 뚫려있다) {
-        반시계방향으로 90' 회전한다.
-        반시계방향으로 90' 회전한다.
-        한칸 앞으로 간다.
-        시간을 +1한다.
-    } else {
-        -1을 출력한다.
-        프로그램을 종료한다.
-    }
-
-    if (범위 밖이다) {
-        시간을 출력한다.
-        프로그램을 종료한다.
-    }
+// 격자를 빠져나오기 전까지 계속 반복합니다.
+while (inRange(currX, currY)) {
+    // 조건에 맞춰 움직여봅니다.
+    simulate();
 }
-*/
+
+console.log(elapsedTime);
