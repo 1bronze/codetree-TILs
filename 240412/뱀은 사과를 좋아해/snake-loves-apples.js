1dd4d8f156ec232ac9248e03d314c12f28ec7adf
Dong Kim <khdkkhdd@gmail.com>
2024-04-12 18:13:03 +0900

    Title: 뱀은 사과를 좋아해, Time: 432ms, Memory: 22MB, Status: Passed - CodeTree

--- 240412/뱀은 사과를 좋아해/snake-loves-apples.js
+++ 240412/뱀은 사과를 좋아해/snake-loves-apples.js
@@ -2,10 +2,15 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 // 변수 선언 및 입력
-const [n, m, K] = input.shift().split(' ').map(Number);
+const [n, m, k] = input[0].split(' ').map(Number);
+const positions = input.slice(1, 1 + m).map(line => line.split(' ').map(Number));
+const movements = input.slice(1 + m, 1 + m + k).map(line => line.split(' '));
+
 const apple = Array.from(Array(n + 1), () => Array(n + 1).fill(false));
+
 // 뱀은 처음에 (1, 1)에서 길이 1의 상태로 있습니다.
 let snake = [{x: 1, y: 1}];
+let ans = 0;
 
 // 입력으로 주어진 방향을 정의한 dx, dy에 맞도록
 // 변환하는데 쓰이는 객체를 정의합니다.
@@ -16,9 +21,6 @@ const mapper = {
     'L': 3
 };
 
-let ans = 0;
-
-
 // (x, y)가 범위 안에 들어가는지 확인합니다.
 function canGo(x, y) {
     return 1 <= x && x <= n && 1 <= y && y <= n;
@@ -26,7 +28,17 @@ function canGo(x, y) {
 
 // 뱀이 꼬였는지 확인합니다.
 function isTwisted(newHead) {
-    return snake.some(part => part.x === newHead.x && part.y === newHead.y);
+    // 뱀이 꼬였는지 여부는
+    // 새로 들어올 머리가 기존 뱀의 몸통과 부딪히는지만 확인하면 됩니다.
+    
+	// 머리와 그 부분이 겹치는 경우에는
+	// true 값을 반환해줍니다.
+    for(let i = 0; i < snake.length; i++)
+        if(newHead.x === snake[i].x && newHead.y === snake[i].y)
+            return true;
+    
+	// 겹치지 않는 경우에는 false를 반환합니다.
+    return false;
 }
 
 // 새로운 머리를 추가합니다.
@@ -81,7 +93,8 @@ function moveSnake(nx, ny) {
 
 // 뱀을 moveDir 방향으로 num번 움직입니다.
 function move(moveDir, num) {
-    const dxs = [1, -1, 0, 0], dys = [0, 0, 1, -1];
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
     
     // num 횟수만큼 뱀을 움직입니다.
     // 한 번 움직일 때마다 답을 갱신합니다.
@@ -91,8 +104,8 @@ function move(moveDir, num) {
         // 뱀의 머리가 그다음으로 움직일
         // 위치를 구합니다.
         const {x: headX, y: headY} = snake[0];
-        const nx = headX + dxs[moveDir];
-        const ny = headY + dys[moveDir];
+        const nx = headX + dx[moveDir];
+        const ny = headY + dy[moveDir];
         
         // 그 다음 위치로 갈 수 없다면
         // 게임을 종료합니다.
@@ -112,20 +125,16 @@ function move(moveDir, num) {
 }
 
 // 사과가 있는 위치를 표시합니다.
-for (let i = 0; i < m; i++) {
-    const [x, y] = input.shift().split(' ').map(Number);
+positions.forEach(([x, y]) => {
     apple[x][y] = true;
-}
+})
 
-// K개의 명령을 수행합니다.
-for (let i = 0; i < K; i++) {
+// k개의 명령을 수행합니다.
+for (let [moveDir, num] of movements) {
     // moveDir 방향으로 num 횟수 만큼 움직여야 합니다.
-    let [moveDir, num] = input.shift().split(' ');
-    num = parseInt(num);
-    
     // 움직이는 도중 게임이 종료되었을 경우
     // 더 이상 진행하지 않습니다.
-    if (!move(mapper[moveDir], num)) {
+    if (!move(mapper[moveDir], Number(num))) {
         break;
     }
 }
