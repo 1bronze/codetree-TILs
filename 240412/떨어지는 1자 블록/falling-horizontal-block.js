5f078175bd90aad75c16b86c64e7f49a423bf419
Dong Kim <khdkkhdd@gmail.com>
2024-04-12 12:40:32 +0900

    Title: 떨어지는 1자 블록, Time: 235ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240412/떨어지는 1자 블록/falling-horizontal-block.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m, k] = input[0].split(' ').map(Number);
+const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+function check(y, x, len) {
+    if (y === n - 1) return false;
+
+    let flag = true;
+    for (let i = x; i < x + len; i++)
+        if (grid[y + 1][i] !== 0)
+            flag = false;
+    
+    return flag;
+}
+
+function drop(y, x, len) {
+    for (let i = x; i < x + len; i++) {
+        grid[y + 1][i] = 1;
+        grid[y][i] = 0;
+    }
+}
+
+let r = 0;
+while (check(r, k - 1, m)) 
+    drop(r++, k - 1, m)
+
+for (let i = 0; i < n; i++)
+    console.log(grid[i].join(' '));
