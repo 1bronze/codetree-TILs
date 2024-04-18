fc37eb7e09a1c1df255a55cea169099d1126263a
Dong Kim <khdkkhdd@gmail.com>
2024-04-18 17:45:11 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 805ms, Memory: 20MB, Status: Passed - CodeTree

--- 240418/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.js
+++ 240418/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const [k, n] = input[0].split(' ').map(Number);
-const arr = Array.from(Array(n).fill(0));
+let selectedNums = [];
 
-function recur(idx) {
-    if (idx === n - 1) {
-        console.log(arr.join(' '));
+// 선택된 원소들을 출력해줍니다.
+function printPermutation() {
+    console.log(selectedNums.join(' '));
+}
+
+function findPermutations(cnt) {
+    // n개를 모두 뽑은 경우 답을 출력해줍니다.
+    if (cnt === n) {
+        printPermutation();
         return;
     }
-
+    
+    // 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
     for (let i = 1; i <= k; i++) {
-        arr[idx + 1] = i;
-        recur(idx + 1);
+        selectedNums.push(i);
+        findPermutations(cnt + 1);
+        selectedNums.pop();
     }
 }
 
-for (let i = 1; i <= k; i++) {
-    arr[0] = i;
-    recur(0);
-}
+findPermutations(0);
