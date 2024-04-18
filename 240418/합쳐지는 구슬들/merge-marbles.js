2e41e1b25b8672e3b76bfa4dc2a6dc9c92fc5829
Dong Kim <khdkkhdd@gmail.com>
2024-04-18 15:23:13 +0900

    Title: 합쳐지는 구슬들, Time: 241ms, Memory: 20MB, Status: Passed - CodeTree

--- 240418/합쳐지는 구슬들/merge-marbles.js
+++ 240418/합쳐지는 구슬들/merge-marbles.js
@@ -1,120 +1,138 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const BLANK = 0;
-const dy = [-1, 0, 0, 1];
-const dx = [0, 1, -1, 0];
+const EMPTY = [0, 0, 0];
 
-const mapper = {
+const dirMapper = {
     'U': 0,
     'R': 1,
     'L': 2,
     'D': 3,
 }
 
+// 변수 선언 및 입력:
 const [n, m, t] = input[0].split(' ').map(Number);
-const marbles = [0];
-const grid = Array.from(Array(n + 1), () => Array(n + 1).fill(BLANK));
-const nextGrid = Array.from(Array(n + 1), () => Array(n + 1).fill(BLANK));
+const grid = Array.from(Array(n + 1), () => Array(n + 1).fill(EMPTY));
+const nextGrid = Array.from(Array(n + 1), () => Array(n + 1).fill(EMPTY));
 
-function inRange(y, x) {
-    return 1 <= y && y <= n && 1 <= x && x <= n;
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-function resolveCollision(y, x, marble, targetMarble) {
-    // 충돌 시 무게는 두 구슬의 합이 된다.
-    let newWeight = marble.w + targetMarble.w;
-    let newIndex = Math.max(marble.i, targetMarble.i);
-    let newDirection = (newIndex === marble.i) ? marble.d : targetMarble.d;
+function nextPos(x, y, moveDir) {
+    // 방향 전환을 쉽게 하기 위해 
+    // dx, dy 테크닉에서 0<->3, 1<->2가
+    // 서로 반대 방향이 되도록 정의합니다.
+    const dxs = [-1, 0, 0, 1];
+    const dys = [0, 1, -1, 0];
 
-    // 구슬 정보를 업데이트한다.
-    marbles[newIndex].w = newWeight;
-    marbles[newIndex].d = newDirection;
+    let nx = x + dxs[moveDir];
+    let ny = y + dys[moveDir];
 
-    nextGrid[y][x] = newIndex;
-}
-
-function move(y, x, marbleIdx) {
-    // 찾은 구슬의 정보를 가져온다.
-    let marble = marbles[marbleIdx];
-    
-    // 구슬이 다음으로 이동할 위치를 찾는다.
-    let ny = y + dy[marble.d];
-    let nx = x + dx[marble.d];
-
-    // 다음 위치가 범위 밖이면
-    if (!inRange(ny, nx)) {
-        // 방향을 바꾸고 움직이는 것을 마친다.
-        marble.d = 3 - marble.d;
-
-        if (nextGrid[y][x] === BLANK) {
-            nextGrid[y][x] = marbleIdx;
-        } else {
-            let targetMarble = marbles[nextGrid[y][x]];
-            resolveCollision(y, x, marble, targetMarble);
-        }
-        return;
-    }
-
-    // 다음 위치가 비어 있으면
-    if (nextGrid[ny][nx] === BLANK) {
-        // 다음 위치로 이동한다.
-        nextGrid[ny][nx] = marbleIdx;
+    // 벽에 부딪히게 된다면, 방향만 전환해줍니다.
+    if (!inRange(nx, ny)) {
+        moveDir = 3 - moveDir;
     } 
-    // 다음 위치에 이미 다른 구슬이 존재하면
+    // 그렇지 않다면, 한 칸 전진합니다.
     else {
-        let targetMarble = marbles[nextGrid[ny][nx]];
-        // 충돌을 해결하면서 움직인다.
-        resolveCollision(ny, nx, marble, targetMarble);
+        x = nx;
+        y = ny;
     }
+
+    return [x, y, moveDir];
+}
+
+// (x, y) 위치에 새로운 구슬이 들어왔을 때 갱신을 진행합니다.
+function update(x, y, newMarble) {
+    // 기존 구슬 정보입니다.
+    let [num, weight, moveDir] = nextGrid[x][y];
+
+    // 새롭게 들어온 구슬 정보입니다.
+    let [newNum, newWeight, newDir] = newMarble;
+
+    // 새로 들어온 구슬이 더 우선순위가 높다면
+    // 번호와 방향은 새로운 구슬을 따르게 됩니다.
+    if (newNum > num)
+        nextGrid[x][y] = [newNum, weight + newWeight, newDir];
+    // 기존 구슬의 우선순위가 더 높다면
+    // 무게만 더해집니다.
+    else
+        nextGrid[x][y] = [num, weight + newWeight, moveDir];
+}
+
+function move(x, y) {
+    let [num, weight, moveDir] = grid[x][y];
+
+    // Step1. 현재 구슬의 다음 위치와 방향을 구합니다.
+    let [nx, ny, nextDir] = nextPos(x, y, moveDir);
+
+    // Step2. 구슬을 옮겨줍니다.
+    update(nx, ny, [num, weight, nextDir]);
 }
 
 function simulate() {
-    // 구슬이 있는 곳을 모두 찾아다니면서
-    for (let y = 1; y <= n; y++) {
-        for (let x = 1; x <= n; x++) {
-            if (grid[y][x] === BLANK) continue;
-
-            // 구슬을 이동시킨다.
-            const marbleIdx = grid[y][x];
-            move(y, x, marbleIdx);
+    // Step1. nextGrid를 초기화합니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            nextGrid[i][j] = EMPTY;
         }
     }
 
-    for (let y = 1; y <= n; y++) {
-        for (let x = 1; x <= n; x++) {
-            grid[y][x] = nextGrid[y][x];
-            nextGrid[y][x] = BLANK;
+    // Step2. 각 구슬들을 한 칸씩 움직여줍니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] !== EMPTY) {
+                move(i, j);
+            }
         }
     }
+
+    // Step3. nextGrid 값을 grid로 옮겨줍니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            grid[i][j] = nextGrid[i][j];
+        }
+    }
+}
+
+function getMarbleNum() {
+    let cnt = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] !== EMPTY) {
+                cnt += 1;
+            }
+        }
+    }
+    return cnt;
+}
+
+function getMaxWeight() {
+    let maxWeight = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] !== EMPTY) {
+                const [, weight,] = grid[i][j];
+                maxWeight = Math.max(maxWeight, weight);
+            }
+        }
+    }
+    return maxWeight;
 }
 
 for (let i = 1; i <= m; i++) {
     let [r, c, d, w] = input[i].split(' ');
 
     [r, c, w] = [r, c, w].map(Number);
-    d = mapper[d];
+    d = dirMapper[d];
 
-    grid[r][c] = i;
-    marbles.push({
-        i: i,
-        d: d,
-        w: w
-    });
+    grid[r - 1][c - 1] = [i, w, d];
 }
 
+// t초에 걸쳐 시뮬레이션을 진행합니다.
 for (let i = 1; i <= t; i++)
     simulate();
 
-let cnt = 0;
-let largeWeight = 0;
-for (let y = 1; y <= n; y++) {
-    for (let x = 1; x <= n; x++) {
-        if (grid[y][x] === BLANK) continue;
-
-        cnt++;
-        largeWeight = Math.max(largeWeight, marbles[grid[y][x]].w);
-    }
-}
-
-console.log(cnt, largeWeight);
+const marbleNum = getMarbleNum();
+const maxWeight = getMaxWeight();
+console.log(marbleNum, maxWeight);
