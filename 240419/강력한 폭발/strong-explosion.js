3efeaf2cfb2e0317ee450ab284abffc89ae4f103
Dong Kim <khdkkhdd@gmail.com>
2024-04-19 10:40:53 +0900

    Title: 강력한 폭발, Time: 471ms, Memory: 26MB, Status: Passed - CodeTree

--- 240419/강력한 폭발/strong-explosion.js
+++ 240419/강력한 폭발/strong-explosion.js
@@ -1,78 +1,92 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const n = Number(input[0]);
-const grid = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
-const isBomb = Array.from(Array(n), () => Array(n).fill(false));
-const bombs = [0];
+const bombType = Array.from(Array(n), () => Array(n).fill(0));
+const bombed = Array.from(Array(n), () => Array(n).fill(false));
 
 let ans = 0;
+const bombPos = [];
 
-function init() {
-    // let i = 0;
-    for (let y = 0; y < n; y++)
-        for (let x = 0; x < n; x++)
-            if (grid[y][x] === 1)
-                bombs.push({y: y, x: x});
-    // i++;
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-function inRange(y, x) {
-    return y >= 0 && y < n && x >= 0 && x < n;
+function bomb(x, y, bType) {
+    // 폭탄 종류마다 터질 위치를 미리 정의합니다.
+    const bombShapes = [
+        [],
+        [[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
+        [[-1, 0], [1, 0], [0, 0], [0, -1], [0, 1]],
+        [[-1, -1], [-1, 1], [0, 0], [1, -1], [1, 1]]
+    ];
+
+    // 격자 내 칸에 대해서만 영역을 표시합니다.
+    for (let i = 0; i < 5; i++) {
+        const [dx, dy] = bombShapes[bType][i];
+        const [nx, ny] = [x + dx, y + dy];
+        if (inRange(nx, ny)) {
+            bombed[nx][ny] = true;
+        }
+    }
 }
 
-function setBomb() {
-    const types = [[[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
-                   [[0, 0], [-1, 0], [0, 1], [1, 0], [0, -1]],
-                   [[0, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]]
+function calc() {
+    // Step1. 폭탄이 터진 위치를 표시하는 배열을 초기화합니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            bombed[i][j] = false;
+        }
+    }
 
-    for (let y = 0; y < n; y++)
-        for (let x = 0; x < n; x++)
-            isBomb[y][x] = false;
-
-    for (let y = 0; y < n; y++) {
-        for (let x = 0; x < n; x++) {
-            if (grid[y][x] === 0) continue;
-
-            for (let i = 0; i < 5; i++) {
-                const ny = y + types[grid[y][x] - 2][i][0];
-                const nx = x + types[grid[y][x] - 2][i][1];
-
-                if (!inRange(ny, nx)) continue;
-                isBomb[ny][nx] = true;
+    // Step2. 각 폭탄의 타입에 따라 초토화 되는 영역을 표시합니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (bombType[i][j]) {
+                bomb(i, j, bombType[i][j]);
             }
         }
     }
+
+    // Step3. 초토화된 영역의 수를 구합니다.
+    let cnt = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (bombed[i][j]) {
+                cnt += 1;
+            }
+        }
+    }
+
+    return cnt;
 }
 
-function countBomb() {
-    let ret = 0;
-    for (let y = 0; y < n; y++)
-        for (let x = 0; x < n; x++)
-            if (isBomb[y][x])
-                ret++;
-
-    return ret;
-}
-
-function recur(cnt) {
-    if (cnt >= bombs.length - 1) {
-        setBomb();
-        ans = Math.max(ans, countBomb());
+function findMaxArea(cnt) {
+    if (cnt === bombPos.length) {
+        ans = Math.max(ans, calc());
         return;
     }
 
-    // for (let i = cnt + 1; i < bombs.length; i++) {
-        let ny = bombs[cnt + 1].y;
-        let nx = bombs[cnt + 1].x;
+    for (let i = 1; i <= 3; i++) {
+        const [x, y] = bombPos[cnt];
+        bombType[x][y] = i;
+        findMaxArea(cnt + 1);
+        bombType[x][y] = 0;
+    }
+}
 
-        for (let j = 2; j <= 4; j++) {
-            grid[ny][nx] = j;
-            recur(cnt + 1);
+let rowIdx = 1;
+for (let i = 0; i < n; i++) {
+    const givenRow = input[rowIdx++].split(' ').map(Number);
+    for (let j = 0; j < n; j++) {
+        const bombPlace = givenRow[j];
+        if (bombPlace) {
+            bombPos.push([i, j]);
         }
     }
-// }
+}
 
-init();
-recur(0);
+findMaxArea(0);
+
 console.log(ans);
