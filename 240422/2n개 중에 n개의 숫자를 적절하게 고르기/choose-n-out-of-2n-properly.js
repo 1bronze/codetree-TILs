ea59ae3c14e539b3b59bee1ab5c3f26a0d1bdf23
Dong Kim <khdkkhdd@gmail.com>
2024-04-22 11:44:05 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 252ms, Memory: 15MB, Status: Passed - CodeTree

--- 240422/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.js
+++ 240422/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.js
@@ -6,37 +6,22 @@ const INT_MAX = Number.MAX_SAFE_INTEGER;
 // 변수 선언 및 입력
 const n = Number(input[0]);
 const num = input[1].split(' ').map(Number);
-const visited = Array(2 * n).fill(false);
 
 let ans = INT_MAX;
 
-function calc() {
-    let diff = 0;
-    for (let i = 0; i < 2 * n; i++) {
-        diff = visited[i] ? (diff + num[i]) : (diff - num[i]);
-    }
-    
-    return Math.abs(diff);
-}
-
-function findMin(idx, cnt) {
-    if (cnt === n) {
-        ans = Math.min(ans, calc());
-        return;
-    }
-    
+function findMin(idx, cnt, diff) {
     if (idx === 2 * n) {
+        if (cnt === n) {
+            ans = Math.min(ans, Math.abs(diff));
+        }
         return;
     }
     
     // 현재 숫자를 첫 번째 그룹에 사용한 경우입니다.
-    visited[idx] = true;
-    findMin(idx + 1, cnt + 1);
-    visited[idx] = false;
-    
-    // 현재 숫자를 두 번째 그룹에 사용한 경우입니다.
-    findMin(idx + 1, cnt);
+    findMin(idx + 1, cnt + 1, diff + num[idx]);
+    // 현재 숫자를 두 번째 그룹에 사용한 경우입니다.  
+    findMin(idx + 1, cnt, diff - num[idx]);
 }
 
-findMin(0, 0);
+findMin(0, 0, 0);
 console.log(ans);
