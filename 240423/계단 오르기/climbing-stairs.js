8c6c2de5eb201729890c2a7cdd609dbf096af0fc
Dong Kim <khdkkhdd@gmail.com>
2024-04-23 18:52:50 +0900

    Title: 계단 오르기, Time: 206ms, Memory: 14MB, Status: Passed - CodeTree

--- 240423/계단 오르기/climbing-stairs.js
+++ 240423/계단 오르기/climbing-stairs.js
@@ -1,21 +1,17 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const UNUSED = -1;
+const MOD = 10007;
 
 const n = Number(input[0]);
-const dp = Array(n + 1).fill(UNUSED);
-
-function recur(n) {
-    if (dp[n] !== UNUSED)
-        return dp[n];
-
-    return (dp[n - 2] + dp[n - 3]) % 10007;
-}
+const dp = Array(n + 1).fill(0);
 
 dp[0] = 1;
 dp[1] = 0;
 dp[2] = 1;
 dp[3] = 1;
 
-console.log(recur(n));
+for(let i = 4; i <= n; i++)
+    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
+
+console.log(dp[n]);
