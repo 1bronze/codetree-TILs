412d2ada78648a95f8176f5370e3bd9e1918dd9d
Dong Kim <khdkkhdd@gmail.com>
2024-04-23 19:06:18 +0900

    Title: 계단 오르기, Time: 5069ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

--- 240423/계단 오르기/climbing-stairs.js
+++ 240423/계단 오르기/climbing-stairs.js
@@ -2,19 +2,22 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const MOD = 10007;
+const UNUSED = -1;
 
 // 변수 선언 및 입력:
 const n = Number(input[0]);
-const dp = Array(n + 1).fill(0);
+const dp = Array(n + 1).fill(UNUSED);
 
-// 초기 조건 설정
+function recur(n) {
+    if (dp[n] !== UNUSED)
+        return dp[n];
+
+    return (recur(n - 2) + recur(n - 3)) % MOD;
+}
+
 dp[0] = 1;
 dp[1] = 0;
 dp[2] = 1;
 dp[3] = 1;
 
-// 점화식에 따라 dp값 채우기
-for(let i = 4; i <= n; i++)
-    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
-
-console.log(dp[n]);
+console.log(recur(n));
