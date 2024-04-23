be6fae6f5540e53e61d97650050adfe197eee7ff
Dong Kim <khdkkhdd@gmail.com>
2024-04-23 18:41:28 +0900

    Title: 피보나치 수, Time: 307ms, Memory: 14MB, Status: Passed - CodeTree

--- 240423/피보나치 수/fibonacci-number.js
+++ 240423/피보나치 수/fibonacci-number.js
@@ -4,9 +4,9 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const dp = Array(n + 1).fill(0);
 
-dp[1] = dp[2] = 1;
+dp[1] = 1; dp[2] = 1;
 
 for (let i = 3; i <= n; i++)
-    dp[n] = dp[n - 1] + dp[n - 2];
+    dp[i] = dp[i - 1] + dp[i - 2];
 
 console.log(dp[n]);
