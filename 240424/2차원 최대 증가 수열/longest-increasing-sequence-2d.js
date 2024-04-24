a30808eefefdb772c88c7af48496322e11df66b6
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 18:18:17 +0900

    Title: 2차원 최대 증가 수열, Time: 278ms, Memory: 15MB, Status: Passed - CodeTree

--- 240424/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
+++ 240424/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -1,28 +1,66 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+const INT_MIN = Number.MIN_SAFE_INTEGER;
+
+// 변수 선언 및 입력
 const [n, m] = input[0].split(' ').map(Number);
-const num = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number));
-const dp = Array.from(Array(n + 1), () => Array(m + 1).fill(0));
+const grid = input.slice(1, n + 1).map(row => row.trim().split(' ').map(Number));
+const dp = Array.from(Array(n), () => Array(m).fill(0));
 
+function initialize() {
+    // 최대를 구해야 하므로
+    // 전부 INT_MIN으로 초기화를 합니다.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            dp[i][j] = INT_MIN;
+        }
+    }
 
-dp[0][0] = 1;
+    // 초기조건으로는
+    // 시작 위치에 대한 처리가 필요합니다.
+    // 시작 위치에서는 정확히 1칸을 밟게 되는 것이므로
+    // dp[0][0] = 1이 됩니다.
+    dp[0][0] = 1;
+}
 
-for (let x = 1; x < m; x++) {
-    for (let y = 1; y < n; y++) {
+// 초기값을 설정합니다.
+initialize();
 
-        for (let j = 0; j < x; j++) {
-            for (let i = 0; i < y; i++) {
-                if (num[y][x] > num[i][j] && dp[i][j] !== 0)
-                    dp[y][x] = Math.max(dp[y][x], dp[i][j] + 1);
+// 점화식에 따라
+// 위에서 아래, 왼쪽에서 오른쪽 방향으로 진행하며
+// 값을 채워줍니다.
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+        // 바로 직전 위치를 (k, l)이라 했을 때
+        // 조건을 만족하는 경우에 대해
+        // 값을 갱신해줍니다.
+        for (let k = 0; k < i; k++) {
+            for (let l = 0; l < j; l++) {
+                // (k, l)에 도달하는 것이 불가하다면
+                // 패스합니다.
+                if (dp[k][l] === INT_MIN) {
+                    continue;
+                }
+
+                // 값이 증가하는 경우에만
+                // 갱신을 진행합니다.
+                if (grid[k][l] < grid[i][j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[k][l] + 1);
+                }
             }
         }
     }
 }
 
-let ans = 0;
-for (let x = 0; x < m; x++)
-    for (let y = 0; y < n; y++)
-        ans = Math.max(ans, dp[y][x]);
+// 어디에 도착하던 상관없이
+// 최대로 밟을 수 있는 것이 중요하므로
+// 전체 중 최댓값이 답이 됩니다.
+let ans = INT_MIN;
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+        ans = Math.max(ans, dp[i][j]);
+    }
+}
 
 console.log(ans);
