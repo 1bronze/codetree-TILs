e0f37eab025061f8bdba47d25a0f04710320b9e3
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 10:33:23 +0900

    Title: 사각형 채우기 3, Time: 263ms, Memory: 18MB, Status: Passed - CodeTree

--- 240424/사각형 채우기 3/rectangle-fill-3.js
+++ 240424/사각형 채우기 3/rectangle-fill-3.js
@@ -2,30 +2,24 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const MOD = 1000000007;
-const UNUSED = -1;
 
 const n = Number(input[0]);
-const dp = Array(n + 1).fill(UNUSED);
-
-function recur(n) {
-    if (dp[n] !== UNUSED)
-        return dp[n];
-
-    let ret = 0;
-    ret += recur(n - 1) * 2;
-    ret += recur(n - 2) * 3;
-
-    for (let i = n - 3; i >= 0; i -= 2)
-        ret += recur(i) * 2;
-    for (let i = n - 4; i >= 0; i -= 2)
-        ret += recur(i) * 2;
-
-    return ret;
-}
-
+const dp = Array(n + 1).fill(0);
 
 dp[0] = 1;
 dp[1] = 2;
 dp[2] = 7;
 
-console.log(recur(n));
+for (let i = 3; i <= n; i++) {
+    dp[i] += dp[i - 1] * 2;
+    dp[i] += dp[i - 2] * 3;
+
+    for (let j = i - 3; j >= 0; j -= 2)
+        dp[i] += dp[j] * 2;
+    for (let j = i - 4; j >= 0; j -= 2)
+        dp[i] += dp[j] * 2;
+
+    dp[i] %= MOD;
+}
+
+console.log(dp[n]);
