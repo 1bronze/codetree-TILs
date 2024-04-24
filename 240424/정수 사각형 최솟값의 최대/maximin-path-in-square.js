1fa31bfd534cb23caddd590efeb71104bd004bcc
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 13:38:23 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 193ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240424/정수 사각형 최솟값의 최대/maximin-path-in-square.js
+++ 240424/정수 사각형 최솟값의 최대/maximin-path-in-square.js
@@ -1,20 +1,46 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 변수 선언 및 입력:
 const n = Number(input[0]);
-const num = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
-const dp = Array.from(Array(n), () => Array(n).fill(10000000));
 
-dp[0][0] = num[0][0];
-for (let i = 1; i < n; i++) {
-    dp[i][0] = Math.min(dp[i - 1][0], num[i][0]);
-    dp[0][i] = Math.min(dp[0][i - 1], num[0][i]);
+const UNUSED = -1;
+
+const grid = input.map(line => line.split(' ').map(Number));
+const memo = Array.from(Array(n), () => Array(n).fill(UNUSED));
+
+function inRange(x, y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-for (let i = 1; i < n; i++)
-    for (let j = 1; j < n; j++)
-        dp[i][j] = Math.min(num[i][j], Math.max(dp[i - 1][j], dp[i][j - 1]))
-        
+function findMaximin(x, y) {
+    // 미리 계산된 적이 있는 경우 해당 값을 사용해줍니다.
+    if (memo[x][y] !== UNUSED) {
+        return memo[x][y];
+    }
 
-console.log(dp[n - 1][n - 1]);
+    // 도착 지점에 도착하면 최대 합을 갱신해줍니다.
+    if (x === n - 1 && y === n - 1) {
+        memo[n - 1][n - 1] = grid[n - 1][n - 1];
+        return grid[n - 1][n - 1];
+    }
+    
+    const dx = [1, 0], dy = [0, 1];
+    
+    // 가능한 방향에 대해 탐색해줍니다.
+    let maximin = 0;
+    for (let i = 0; i < dx.length; i++) {
+        const new_x = x + dx[i], new_y = y + dy[i];
+        
+        if (inRange(new_x, new_y)) {
+            maximin = Math.max(maximin,
+                Math.min(findMaximin(new_x, new_y), grid[x][y]));
+        }
+    }
+    
+    // 계산된 값을 memo 배열에 저장해줍니다.
+    memo[x][y] = maximin;
+    
+    return maximin;
+}
+
+console.log(findMaximin(0, 0));
