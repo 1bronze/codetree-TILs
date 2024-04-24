0c760b3d19778cf4a80367b18cd95f53d0b77a69
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 13:00:50 +0900

    Title: 정수 사각형 최대 합, Time: 244ms, Memory: 16MB, Status: Passed - CodeTree

--- 240424/정수 사각형 최대 합/maximum-sum-path-in-square.js
+++ 240424/정수 사각형 최대 합/maximum-sum-path-in-square.js
@@ -4,29 +4,38 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const grid = input.slice(1).map(line => line.split(' ').map(Number));
 
+const UNUSED = -1;
+const memo = Array.from({ length: n }, () => Array(n).fill(UNUSED));
+
 function inRange(x, y) {
     return 0 <= x && x < n && 0 <= y && y < n;
 }
 
 function findMaxSum(x, y) {
+    // 미리 계산된 적이 있는 경우 해당 값을 사용해줍니다.
+    if (memo[x][y] !== UNUSED) {
+        return memo[x][y];
+    }
+    
     // 도착 지점에 도착하면 최대 합을 갱신해줍니다.
     if (x === n - 1 && y === n - 1) {
         return grid[n - 1][n - 1];
     }
-    
-    const dx = [1, 0], dy = [0, 1];
 
+    const dx = [1, 0], dy = [0, 1];
+    
     // 가능한 모든 방향에 대해 탐색해줍니다.
-    let maxSum = 0; // 주어진 숫자의 범위가 1보다 크기 때문에 항상 갱신됨이 보장됩니다.
+    let maxSum = 0;
     for (let i = 0; i < dx.length; i++) {
         const newX = x + dx[i], newY = y + dy[i];
         
         if (inRange(newX, newY)) {
-            // 각 방향으로 진행했을 때의 최대 합과 현재 위치의 값을 더합니다.
             maxSum = Math.max(maxSum, findMaxSum(newX, newY) + grid[x][y]);
         }
     }
     
+    // 계산된 값을 memo 배열에 저장해줍니다.
+    memo[x][y] = maxSum;
     return maxSum;
 }
 
