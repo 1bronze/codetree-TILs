46bcb14e20b37f2b9cb8b38c8f85e39eb2068882
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 11:10:45 +0900

    Title: 사각형 채우기 2, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240424/사각형 채우기 2/rectangle-fill-2.js
+++ 240424/사각형 채우기 2/rectangle-fill-2.js
@@ -7,10 +7,13 @@ const MOD = 10007;
 const n = Number(input[0]);
 const dp = Array(n + 1).fill(0);
 
+// 초기 조건 설정
 dp[0] = 1;
 dp[1] = 1;
 
+// 점화식에 따라 dp값 채우기
+// dp[i] = dp[i - 1] + 2 * dp[i - 2]
 for (let i = 2; i<= n; i++)
-    dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD;
+    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD;
 
 console.log(dp[n]);
