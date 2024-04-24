b47be8e5213423bd7d7eb1274eda9ec82ac8f730
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 12:34:07 +0900

    Title: 서로 다른 BST 개수 세기, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 240424/서로 다른 BST 개수 세기/number-of-unique-bst.js
+++ 240424/서로 다른 BST 개수 세기/number-of-unique-bst.js
@@ -3,26 +3,23 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 
-const UNUSED = -1;
-const memo = Array(n + 1).fill(UNUSED);
+const MAX_NUM = 19;
+const dp = Array(MAX_NUM + 1).fill(0);
 
 function getNumOfUniqueBst(n) {
-    if (memo[n] !== UNUSED) {
-        return memo[n];
-    }
-    
-    if (n <= 1) {
-        return 1;
-    }
-    
     let numOfUniqueBst = 0;
+    
     for (let i = 0; i < n; i++) {
-        numOfUniqueBst += getNumOfUniqueBst(i) * 
-                          getNumOfUniqueBst(n - i - 1);
+        numOfUniqueBst += dp[i] * dp[n - i - 1];
     }
     
-    memo[n] = numOfUniqueBst;
-    return memo[n];
+    return numOfUniqueBst;
 }
 
-console.log(getNumOfUniqueBst(n));
+dp[0] = dp[1] = 1;
+
+for (let i = 2; i <= n; i++) {
+    dp[i] = getNumOfUniqueBst(i);
+}
+
+console.log(dp[n]);
