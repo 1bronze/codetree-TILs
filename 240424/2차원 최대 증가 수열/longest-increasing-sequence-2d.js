8eb234f7acb0c2e07208c404bb2af9e27b50bf73
Dong Kim <khdkkhdd@gmail.com>
2024-04-24 18:01:14 +0900

    Title: 2차원 최대 증가 수열, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240424/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
+++ 240424/2차원 최대 증가 수열/longest-increasing-sequence-2d.js
@@ -6,11 +6,7 @@ const num = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number)
 const dp = Array.from(Array(n + 1), () => Array(m + 1).fill(0));
 
 
-for (let i = 0; i < n; i++)
-    dp[i][0] = 1;
-
-for (let i = 0; i < m; i++)
-    dp[0][i] = 1;
+dp[0][0] = 1;
 
 for (let x = 1; x < m; x++) {
     for (let y = 1; y < n; y++) {
