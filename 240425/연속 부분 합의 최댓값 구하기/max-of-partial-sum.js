01f476da74ce11652b551c76bda0089b63ceffb6
Dong Kim <khdkkhdd@gmail.com>
2024-04-25 17:54:49 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 264ms, Memory: 26MB, Status: Passed - CodeTree

--- 240425/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
+++ 240425/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
@@ -7,37 +7,47 @@ const INT_MIN = Number.MIN_SAFE_INTEGER;
 const n = Number(input[0]);
 const a = [0].concat(input[1].trim().split(' ').map(Number));
 
-// prefixSum[i] : 1번째부터 i번째까지 a배열 원소의 합을 저장하고 있습니다.
-const prefixSum = new Array(n + 1).fill(0);
+// dp[i] : 선택한 연속 부분 수열의 마지막 원소의 위치가 i라 했을 때,
+//         얻을 수 있는 최대 합
+const dp = new Array(n + 1).fill(0);
 
-// 누적합 배열에 적절한 값을 채워줍니다.
-function preprocess() {
-    prefixSum[1] = a[1];
-
-    for (let i = 2; i <= n; i++) {
-        prefixSum[i] = prefixSum[i - 1] + a[i];
+function initialize() {
+    // 최댓값을 구하는 문제이므로, 
+    // 초기에는 전부 INT_MIN을 넣어줍니다.
+    for (let i = 1; i <= n; i++) {
+        dp[i] = INT_MIN;
     }
+    
+    // 첫 번째 원소를 연속 부분 수열의 원소로 사용하는 경우를 
+    // 초기 조건으로 설정합니다.
+    // 이때는, 이 원소만 연속 부분 수열에 속하게 되므로 
+    // dp[1] = a[1]이 됩니다.
+    dp[1] = a[1];
 }
 
-preprocess();
+initialize();
 
-// 최댓값을 구해야 하는 문제이므로 초기값을 INT_MIN으로 설정합니다.
+// 선택한 연속 부분 수열의 마지막 원소의 위치가 i라 했을 때, 
+// 얻을 수 있는 최대 합을 계산합니다.
+for (let i = 2; i <= n; i++) {
+    // 이렇 상황을 만들기 위한 선택지는 크게 2가지 입니다.
+    
+    // Case 1
+    // 그 이전 연속 부분 수열에 i번째 원소를 
+    // 더 추가하는 경우입니다.
+    // 추가를 위해서는 정확히 i - 1번째로 끝나는 연속 부분 수열 
+    // 중 최대 합이 필요하므로, dp[i - 1] + a[i]가 
+    // 하나의 선택지가 됩니다.
+    
+    // Case 2
+    // i 번째 원소부터 연속 부분 수열을 만들기 시작하는 경우입니다.
+    // 이 경우에는 원소가 a[i] 하나 뿐이므로, a[i]가 
+    // 또 다른 선택지가 됩니다.
+    dp[i] = Math.max(dp[i - 1] + a[i], a[i]);
+}
+
 let ans = INT_MIN;
-
-// 모든 연속 부분수열 쌍에 대해 그들의 합 중 최댓값을 계산합니다.
-// 이를 0 <= index1 < index2 <= n 를 만족하는 두 위치
-// index1, index2를 골라 누적합의 차가 최대가 되도록 하는
-// 문제로 해결이 가능하므로, 
-// index2를 먼저 고정하고 index1은 index2 앞의 원소들 중
-// 가장 작은 원소를 골라야 차이를 최대화 할 수 있으므로
-// index2가 바뀜에따라 계속 최솟값을 O(1) 시간에 갱신하면서
-// 나아갈 수 있습니다.
-let index1 = 0;
-for (let index2 = 1; index2 <= n; index2++) {
-    ans = Math.max(ans, prefixSum[index2] - prefixSum[index1]);
-    if (prefixSum[index1] > prefixSum[index2]) {
-        index1 = index2;
-    }
-}
+for (let i = 1; i <= n; i++)
+    ans = Math.max(ans, dp[i]);
 
 console.log(ans);
