0f21b3c4dde572c6844714f29135cd9ecf2399b2
Dong Kim <khdkkhdd@gmail.com>
2024-04-25 17:50:27 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 301ms, Memory: 25MB, Status: Passed - CodeTree

--- 240425/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
+++ 240425/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
@@ -19,21 +19,24 @@ function preprocess() {
     }
 }
 
-// 배열 a의 i번째 원소부터 j번째 원소까지의 합을 반환합니다.
-function sumInRange(i, j) {
-    return prefixSum[j] - prefixSum[i] + a[i];
-}
-
 preprocess();
 
 // 최댓값을 구해야 하는 문제이므로 초기값을 INT_MIN으로 설정합니다.
 let ans = INT_MIN;
 
 // 모든 연속 부분수열 쌍에 대해 그들의 합 중 최댓값을 계산합니다.
-// 연속 부분수열이 i로 시작해서 j로 끝나는 (i, j)쌍을 전부 조사해야 합니다.
-for (let i = 1; i <= n; i++) {
-    for (let j = i; j <= n; j++) {
-        ans = Math.max(ans, sumInRange(i, j));
+// 이를 0 <= index1 < index2 <= n 를 만족하는 두 위치
+// index1, index2를 골라 누적합의 차가 최대가 되도록 하는
+// 문제로 해결이 가능하므로, 
+// index2를 먼저 고정하고 index1은 index2 앞의 원소들 중
+// 가장 작은 원소를 골라야 차이를 최대화 할 수 있으므로
+// index2가 바뀜에따라 계속 최솟값을 O(1) 시간에 갱신하면서
+// 나아갈 수 있습니다.
+let index1 = 0;
+for (let index2 = 1; index2 <= n; index2++) {
+    ans = Math.max(ans, prefixSum[index2] - prefixSum[index1]);
+    if (prefixSum[index1] > prefixSum[index2]) {
+        index1 = index2;
     }
 }
 
