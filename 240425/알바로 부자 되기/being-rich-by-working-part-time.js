77df787ae44b7856e6b53a15993dcb15aacf9a0b
Dong Kim <khdkkhdd@gmail.com>
2024-04-25 13:51:11 +0900

    Title: 알바로 부자 되기, Time: 238ms, Memory: 15MB, Status: Passed - CodeTree

--- 240425/알바로 부자 되기/being-rich-by-working-part-time.js
+++ 240425/알바로 부자 되기/being-rich-by-working-part-time.js
@@ -1,29 +1,53 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-// 가치를 DP로 저장?
-// DP[i] = i번째까지 했을 때 최대로 얻을 수 있는 가치
+const INT_MIN = Number.MIN_SAFE_INTEGER;
+
+// 변수 선언 및 입력
 const n = Number(input[0]);
-const arr = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
-const dp = new Array(n).fill(0);
 
-arr.sort((a, b) => a[0] - b[0]);
+// 초기 조건으로 아직 아무런 알바도 진행하지 못했을 경우에 대한
+// 상태를 정의합니다. 편의를 위해 후에
+// 0번째 index와 비교했을 때 항상 갱신될 수
+// 있도록 s[0], e[0], p[0]를 전부 0으로 설정합니다.
+let s = Array(n + 1).fill(0);
+let e = Array(n + 1).fill(0);
+let p = Array(n + 1).fill(0);
 
-dp[0] = arr[0][2];
+// 입력:
+for (let i = 1; i <= n; i++) {
+    [s[i], e[i], p[i]] = input[i].split(' ').map(Number);
+}
 
-for(let i = 1; i < n; i++) {
-    const cur = arr[i];
+// dp[i] : 마지막으로 고른 알바가 i번째 알바일 때,
+// 얻을 수 있었던 최대 수익
+// 최대를 구하는 문제이므로, 초기에는 전부 INT_MIN을 넣어줍니다.
+let dp = Array(n + 1).fill(INT_MIN);
 
+// 마지막으로 고른 알바가 0번째 알바일 때
+// 지금까지 수익이 0이었다는 뜻으로
+// dp[0] = 0을 초기 조건으로 설정합니다.
+dp[0] = 0;
+
+// 마지막으로 고른 알바가 i번째 알바일 때,
+// 얻을 수 있었던 최대 수익을 계산합니다.
+for (let i = 1; i <= n; i++) {
+    // i번째 보다 앞에 있는 알바들 중 (j) i번째와 일하는 기간이
+    // 겹치지 않는 알바들에 대해 해당 알바 다음에 i번째 알바를 진행하는 경우를
+    // 계산하여 그 중 최대 수익을 dp[i]에 적어줍니다.
+    // 알바끼리 겹치지 않기 위해서는 e[j] < s[i] 조건을 만족해야 합니다.
     for (let j = 0; j < i; j++) {
-        const prev = arr[j];
-
-        // Case 1. DP[j]에 더해지는 경우 > 그대로 최댓값
-        // Case 2. DP[j]에 더해질 수 없는 경우 > cur[2] 또는 DP[j]가 최댓값
-        if (cur[0] > prev[1])
-            dp[i] = Math.max(dp[i], dp[j] + cur[2]);
-        else
-            dp[i] = Math.max(dp[i], Math.max(cur[2], dp[j]));
+        if (e[j] < s[i]) {
+            dp[i] = Math.max(dp[i], dp[j] + p[i]);
+        }
     }
 }
 
-console.log(dp[n - 1]);
+// 마지막으로 진행한 알바가 i번째인 경우를
+// 전부 조사하여 그 중 수익을 가장 크게 얻을 수 있는 경우를 선택합니다.    
+let answer = 0;
+for (let i = 0; i <= n; i++) {
+    answer = Math.max(answer, dp[i]);
+}
+
+console.log(answer);
