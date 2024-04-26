5da366ee5835c6f511411645126d8557bacfbbf4
Dong Kim <khdkkhdd@gmail.com>
2024-04-26 17:51:42 +0900

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240426/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int s[1001], e[1001], p[1001], dp[1001];
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> s[i] >> e[i] >> p[i];
+
+    dp[0] = 0;
+    dp[1] = p[1];
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            if (s[i] > e[j])
+                dp[i] = max(dp[i], dp[j] + p[i]);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+    return 0;
+}
