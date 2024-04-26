738143a7b232f1826a4a0d6461249b536770a72d
Dong Kim <khdkkhdd@gmail.com>
2024-04-26 13:11:38 +0900

    Title: 도전! 369게임, Time: 1014ms, Memory: 51MB, Status: Passed - CodeTree

--- 240426/도전! 369게임/challenge-369-game.js
+++ 240426/도전! 369게임/challenge-369-game.js
@@ -1,23 +1,23 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-const MOD = 10**9 + 7;
+const MOD = BigInt(10**9 + 7);
 
 // 문자열 a와 그 길이 n을 입력받습니다.
 const a = input[0].trim();
 const n = a.length;
 
 // 10의 거듭제곱을 미리 계산합니다.
-const pt = Array(n + 1).fill(1);
+const pt = Array(n + 1).fill(1n);
 for (let i = 1; i <= n; i++) {
-    pt[i] = pt[i - 1] * 10 % MOD;
+    pt[i] = pt[i - 1] * 10n % MOD;
 }
 
 // 각 숫자에 대해 동적 프로그래밍을 수행합니다.
 // dp[i][j] :: 앞 i번째 자릿수까지 봤을 때, 각 자릿수의 합이 3의 배수이며, 자릿수 중 3, 6, 9가 나타나지 않았으며,
 // 이 뒤에 0 ~ 9 중 어느 숫자를 붙여도 되는 형태의 자릿수의 개수
-const dp = Array.from(Array(100005), () => Array(5).fill(0));
-let ans = 0;
+const dp = Array.from(Array(100005), () => Array(5).fill(0n));
+let ans = 0n;
 let isSuc = false;
 let sm = 0;
 
@@ -44,7 +44,7 @@ for (let i = 0; i < n; i++) {
             ans += pt[n - i - 1];
             ans %= MOD;
         } else {
-            dp[i + 1][(x + sm) % 3] += 1;
+            dp[i + 1][(x + sm) % 3] += 1n;
             dp[i + 1][(x + sm) % 3] %= MOD;
         }
     }
@@ -59,15 +59,15 @@ for (let i = 0; i < n; i++) {
 
 // 마지막 자릿수를 처리합니다.
 if (isSuc) {
-    ans += 1;
+    ans += 1n;
     ans %= MOD;
 } else {
-    dp[n][sm % 3] += 1;
+    dp[n][sm % 3] += 1n;
     dp[n][sm % 3] %= MOD;
 }
 
 // 최종 결과를 계산하여 출력합니다.
 ans += dp[n][0];
-ans += (MOD - 1);
+ans += (MOD - 1n);
 ans %= MOD;
-console.log(ans);
+console.log(ans.toString());
