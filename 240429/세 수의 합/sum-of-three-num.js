7e3145d2d868475ab5a6ac4e1dabdb8e2c72c0e8
Dong Kim <khdkkhdd@gmail.com>
2024-04-29 15:42:53 +0900

    Title: 세 수의 합, Time: 278ms, Memory: 47MB, Status: Passed - CodeTree

--- 240429/세 수의 합/sum-of-three-num.js
+++ 240429/세 수의 합/sum-of-three-num.js
@@ -4,39 +4,33 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n, k] = input[0].split(' ').map(Number);
 const arr = input[1].split(' ').map(Number);
 
-const count = new Map();
+const freq = new Map();
 let ans = 0;
 
-for (let i = 0; i < arr.length; i++) {
-    const used = new Map();
+// 각 숫자가 몇 번씩 나왔는지를
+// hashmap에 기록해줍니다.
+arr.forEach(elem => {
+    if (freq.has(elem))
+        freq.set(elem, freq.get(elem) + 1);
+    else
+        freq.set(elem, 1);
+})
 
-    const e1 = arr[i];
-    
+// 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+for (let i = 0; i < n; i++) {
+    // 이미 순회한 적이 있는 숫자는 빼 버림으로서
+    // 같은 조합이 여러번 세어지는 걸 방지합니다.
+    if (!freq.has(arr[i]))
+        freq.set(arr[i], -1);
+    else
+        freq.set(arr[i], freq.get(arr[i]) - 1);
+
     for (let j = 0; j < i; j++) {
-        const e2 = arr[j];
-        const e3 = k - arr[i] - arr[j];
-
-        if (used.has(e2) && used.has(e3)) continue;
-
-        if (count.has(e3)) {
-            if (e2 === e3) {
-                ans += Math.floor(count.get(e2) * (count.get(e3) - 1) / 2);
-                used.set(e2, true);
-            } else {
-                ans += count.get(e2) * count.get(e3);
-                used.set(e2, true);
-                used.set(e3, true);
-            }
-        }
+        // 전처리를 해주었기 때문에 이미 순회한 적 있는 값은 hashmap에 없습니다.
+        // 이와 같은 방식으로 같은 조합이 중복되어 세어지는 걸 방지할 수 있습니다.
+        if (freq.has(k - arr[i] - arr[j]))
+            ans += freq.get(k - arr[i] - arr[j]);
     }
-
-    if (count.has(e1)) {
-        count.set(e1, count.get(e1) + 1);
-    } else {
-        count.set(e1, 1);
-    }
-
-    // console.log(i, ans);
 }
 
 console.log(ans);
