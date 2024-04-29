1bb2bd4fd7f950b134c7f1aa1cac245c85886c77
Dong Kim <khdkkhdd@gmail.com>
2024-04-29 17:35:37 +0900

    Title: 원소의 합이 0, Time: 244ms, Memory: 29MB, Status: Passed - CodeTree

--- 240429/원소의 합이 0/the-sum-of-the-elements-is-0.js
+++ 240429/원소의 합이 0/the-sum-of-the-elements-is-0.js
@@ -1,25 +1,40 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력:
 const n = Number(input[0]);
-const arr = input.slice(1, 5).map(line => line.split(' ').map(Number));
+const A = input[1].split(' ').map(Number);
+const B = input[2].split(' ').map(Number);
+const C = input[3].split(' ').map(Number);
+const D = input[4].split(' ').map(Number);
 
-const map1 = new Map();
-const map2 = new Map();
-
-for (let i = 0; i < n; i++)
-    for (let j = 0; j < n; j++)
-        map1.set(arr[0][i] + arr[1][j], (map1.get(arr[0][i] + arr[1][j]) || 0) + 1);
-
-for (let i = 0; i < n; i++)
-    for (let j = 0; j < n; j++)
-        map2.set(arr[2][i] + arr[3][j], (map2.get(arr[2][i] + arr[3][j]) || 0) + 1);
-
+const count = new Map();
 let ans = 0;
 
-map1.forEach((value, key) => {
-    const diff = 0 - key;
-    ans += value * (map2.get(diff) || 0);
-})
+// A 수열에서 숫자 하나, B 수열에서 숫자 하나를 골랐을 때
+// 나올 수 있는 두 숫자의 합들을 hashmap에 기록해줍니다.
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        const sumVal = A[i] + B[j];
+        if (count.has(sumVal))
+            count.set(sumVal, count.get(sumVal) + 1);
+        else 
+            count.set(sumVal, 1);
+    }
+}
+
+// C, D 수열을 순회하며 쌍을 만들어줍니다.
+// 앞서 계산한 hashmap을 이용하면
+// C, D 수열에서 고른 값으로 A, B와 쌍을 만들 때
+// 총합이 0이 되는 쌍의 개수를 쉽게 구할 수 있습니다.
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        const diff = -C[i] - D[j];
+
+        if (count.has(diff))
+            ans += count.get(diff);
+    }
+}
 
 console.log(ans);
