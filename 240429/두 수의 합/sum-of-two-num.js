d2907be2456482f0034b7246182e8c343ef101b9
Dong Kim <khdkkhdd@gmail.com>
2024-04-29 14:05:24 +0900

    Title: 두 수의 합, Time: 255ms, Memory: 33MB, Status: Passed - CodeTree

--- 240429/두 수의 합/sum-of-two-num.js
+++ 240429/두 수의 합/sum-of-two-num.js
@@ -1,28 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
+// 변수 선언 및 입력:
 const [n, k] = input[0].split(' ').map(Number);
-const nums = input[1].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
 
-const freq = new Map();
-
-nums.forEach(num => {
-    if (freq.has(num))
-        freq.set(num, freq.get(num) + 1);
-    else
-        freq.set(num, 1);
-})
-
+const count = new Map();
 let ans = 0;
 
-nums.forEach(num => {
-    const find = k - num;
+// 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+arr.forEach(elem => {
+    const diff = k - elem;
 
-    if (num === find && freq.has(find))
-        ans += freq.get(find) - 1;
-    else if (freq.has(find))
-        ans += freq.get(find);
+    // 가능한 모든 쌍의 수를 세어줍니다.
+    if (count.has(diff))
+        ans += count.get(diff);
+
+    // 현재 숫자의 개수를 하나 증가시켜줍니다.
+    if (count.has(elem))
+        count.set(elem, count.get(elem) + 1);
+    else
+        count.set(elem, 1);
 })
 
-ans = Math.floor(ans / 2);
 console.log(ans);
