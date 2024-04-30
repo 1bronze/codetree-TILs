c549400ffd7b97f55be08171d9ec5a915ed02ab6
Dong Kim <khdkkhdd@gmail.com>
2024-04-30 14:30:06 +0900

    Title: 자리 바꾸기 2, Time: 5041ms, Memory: 49MB, Status: Time Limit Exceed - CodeTree

---
+++ 240430/자리 바꾸기 2/changing-seats-2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const exchanges = input.slice(1, 1 + k).map(line => line.split(' ').map(Number));
+
+const cur = new Array(n + 1);
+const records = Array.from(Array(n + 1), () => new Set());
+
+for (let i = 1; i <= n; i++) {
+    cur[i] = i;
+    records[cur[i]].add(i);
+}
+
+for (let c = 0; c < 3; c++) {
+    exchanges.forEach(([a, b]) => {
+        [cur[a], cur[b]] = [cur[b], cur[a]];
+
+        for (let i = 1; i <= n; i++)
+            records[cur[i]].add(i);
+    });
+}
+
+for (let i = 1; i <= n; i++)
+    console.log(records[i].size);
