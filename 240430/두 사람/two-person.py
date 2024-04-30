b0e048d81ed522b278888bee0d847b82434edf53
Dong Kim <khdkkhdd@gmail.com>
2024-04-30 20:09:21 +0900

    Title: 두 사람, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/두 사람/two-person.py
+++ 240430/두 사람/two-person.py
@@ -3,13 +3,13 @@ c, d = input().split()
 a, c = int(a), int(c)
 
 if b == 'M':
-    if max(a, c) >= 19:
+    if a >= 19:
         print(1)
     else:
         print(0)
 
 elif d == 'M':
-    if max(a, c) >= 19:
+    if c >= 19:
         print(1)
     else:
         print(0)
