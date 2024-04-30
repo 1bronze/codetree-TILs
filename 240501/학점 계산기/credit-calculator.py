409d94e02c7b57e2c0c6f2cb65c162c9ed386b38
Dong Kim <khdkkhdd@gmail.com>
2024-05-01 00:08:00 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/학점 계산기/credit-calculator.py
+++ 240501/학점 계산기/credit-calculator.py
@@ -1,12 +1,12 @@
 n = int(input())
 grade = list(map(float, input().split(" ")))
-av_grade = sum(grade) / n
+# av_grade = sum(grade) / n
 
-print(round(av_grade, 1))
+# print(round(av_grade, 1))
 
-if av_grade >= 4:
-    print("Perfect")
-elif av_grade >= 3:
-    print("Good")
-else:
-    print("Poor")
+# if av_grade >= 4:
+#     print("Perfect")
+# elif av_grade >= 3:
+#     print("Good")
+# else:
+#     print("Poor")
