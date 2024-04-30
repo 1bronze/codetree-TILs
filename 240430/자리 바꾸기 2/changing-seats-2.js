6c028cca725705913e242aba9e4f2965a6dff138
Dong Kim <khdkkhdd@gmail.com>
2024-04-30 14:52:59 +0900

    Title: 자리 바꾸기 2, Time: 1992ms, Memory: 79MB, Status: Passed - CodeTree

--- 240430/자리 바꾸기 2/changing-seats-2.js
+++ 240430/자리 바꾸기 2/changing-seats-2.js
@@ -1,25 +1,46 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 입력:
 const [n, k] = input[0].split(' ').map(Number);
-const exchanges = input.slice(1, 1 + k).map(line => line.split(' ').map(Number));
+const change = input.slice(1, 1 + k).map(line => line.split(' ').map(Number));
 
-const cur = new Array(n + 1);
-const records = Array.from(Array(n + 1), () => new Set());
+// 변수 선언
+const s = Array.from(Array(n + 1), () => new Set());
+const ans = Array(n + 1).fill(0);
+
+// 시작 array를 arr에 표시합니다.
+const arr = Array(n + 1).fill(0);
+for (let i = 1; i <= n; i++)
+    arr[i] = i;
 
 for (let i = 1; i <= n; i++) {
-    cur[i] = i;
-    records[cur[i]].add(i);
+    // 각 i 숫자가 위치할 수 있는 자리를 HashSet에 넣어 저장합니다.
+    s[i].add(i);
+    ans[i] = 1;
 }
 
-for (let c = 0; c < 3; c++) {
-    exchanges.forEach(([a, b]) => {
-        [cur[a], cur[b]] = [cur[b], cur[a]];
+// 3k번 자리를 바꿉니다.
+for (let _ = 0; _ < 3; _++) {
+    change.forEach(([a, b]) => {
+        // a와 b의 자리를 바꿔줍니다.
+        // 이때 자리가 변화한 값은 arr[a]와 arr[b]밖에 없습니다.
+        // 이 값들만 새로운 자리를 HashSet에 넣어 저장합니다.
+        [arr[a], arr[b]] = [arr[b], arr[a]];
 
-        records[cur[a]].add(a);
-        records[cur[b]].add(b);
+        if (!s[arr[a]].has(a)) {
+            s[arr[a]].add(a);
+            ans[arr[a]] += 1;
+        }
+
+        if (!s[arr[b]].has(b)) {
+            s[arr[b]].add(b);
+            ans[arr[b]] += 1;
+        }
     });
 }
 
-for (let i = 1; i <= n; i++)
-    console.log(records[i].size);
+// 3k번 자리를 바꾸면서 각 숫자들이 위치할 수 있는 값들의 개수를 ans 배열에서 확인합니다.
+for (let i = 1; i <= n; i++) {
+    console.log(ans[i]);
+}
