33f70f62d698d77fc82f6e0bb74f1feb31e0fee5
Dong Kim <khdkkhdd@gmail.com>
2024-05-16 14:31:41 +0900

    Title: 친한 점, Time: 1769ms, Memory: 59MB, Status: Wrong Answer - CodeTree

---
+++ 240516/친한 점/frendly-point.js
@@ -0,0 +1,30 @@
+const SortedSet = require("collections/sorted-set");
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+// 변수 선언 및 입력:
+const [n, m] = input[0].split(" ").map(Number);
+const points = input.slice(1, 1 + n).map(line => line.split(" ").map(Number));
+const queries = input.slice(1 + n, 1 + n + m).map(line => line.split(" ").map(Number));
+
+// treeset에 점들을 넣어줍니다.
+const s = new SortedSet(points, (a, b) => {
+    if (a[0] === b[0]) {
+        return a[1] - b[1];
+    }
+    return a[0] - b[0];
+});
+
+// 질의마다 조건에 맞는 점을 찾아줍니다.
+queries.forEach(target => {
+    const idx = s.findLeastGreaterThanOrEqual(target);
+    // 그러한 점이 없다면 -1을 출력합니다.
+    if (!idx) {
+        console.log(-1, -1);
+    }
+    // 존재한다면, 그 지점의 값을 출력합니다.
+    else {
+        const [x, y] = idx.value;
+        console.log(x, y);
+    }
+});
