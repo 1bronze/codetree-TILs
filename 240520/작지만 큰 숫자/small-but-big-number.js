d59f604f279e590de2a28e2b9d774db2da72a5ca
Dong Kim <khdkkhdd@gmail.com>
2024-05-20 11:16:01 +0900

    Title: 작지만 큰 숫자, Time: 2165ms, Memory: 50MB, Status: Passed - CodeTree

--- 240520/작지만 큰 숫자/small-but-big-number.js
+++ 240520/작지만 큰 숫자/small-but-big-number.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 // 입력
-const [n, m] = input[0].split(" ").map(Number);
-const arr = input[1].split(" ").map(Number);
-const arr2 = input[2].split(" ").map(Number);
+const [n, m] = input[0].trim().split(" ").map(Number);
+const arr = input[1].trim().split(" ").map(Number);
+const arr2 = input[2].trim().split(" ").map(Number);
 
 // 변수 선언
 const s = new SortedSet();
@@ -20,7 +20,7 @@ arr2.forEach(elem => {
     if (idx) {
         // 존재하는 경우에는 해당 값을 출력한 뒤 제거합니다.
         console.log(idx.value);
-        s.remove(idx.index);
+        s.remove(idx.value);
     } else {
         // 존재하지 않는 경우에는 -1을 출력합니다.
         console.log(-1);
