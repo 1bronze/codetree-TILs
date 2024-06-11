7f8288a8fc7c9c8e8f83f88c38ca443a7ecd7a72
Dong Kim <khdkkhdd@gmail.com>
2024-06-11 19:13:05 +0900

    Title: 정수 n개의 합 2, Time: 256ms, Memory: 23MB, Status: Passed - CodeTree

--- 240611/정수 n개의 합 2/sum-of-n-integers-2.js
+++ 240611/정수 n개의 합 2/sum-of-n-integers-2.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const [n, k] = input[0].trim().split(' ').map(Number);
-const num = input[1].trim().split(' ').map(Number);
-const sum = new Array(n).fill(0);
+const arr = [0].concat(input[1].trim().split(' ').map(Number));
+const prefixSum = new Array(n).fill(0);
+let ans = Number.MIN_SAFE_INTEGER;
 
-sum[0] = num[0];
-for (let i = 1; i < n; i++)
-    sum[i] = sum[i - 1] + num[i];
+// [s, e] 구간 내의 원소의 합을 반환합니다.
+function getSum(s, e) {
+    return prefixSum[e] - prefixSum[s - 1];
+}
 
-let ans = sum[k - 1];
-for (let i = k; i < n; i++)
-    ans = Math.max(ans, sum[i] - sum[i - k]);
+// 누적합 배열을 만들어줍니다.
+prefixSum[0] = 0;
+for (let i = 1; i <= n; i++)
+    prefixSum[i] = prefixSum[i - 1] + arr[i];
+
+// 모든 구간에 대해 합을 찾아
+// 그 중 최댓값을 갱신합니다.
+for (let i = 1; i < n - k + 2; i++)
+    ans = Math.max(ans, getSum(i, i + k - 1));
 
 console.log(ans);
