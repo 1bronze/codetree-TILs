16716b5abf4c7e1a667e53b1a2e85a9f556934b0
Dong Kim <khdkkhdd@gmail.com>
2024-06-12 13:33:08 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1340ms, Memory: 61MB, Status: Wrong Answer - CodeTree

---
+++ 240612/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력:
+const [n, q] = input[0].trim().split(' ').map(Number);
+const num = input[1].trim().split(' ').map(Number);
+const range = input.slice(2, 2 + q).map(line => line.trim().split(' ').map(Number));
+
+const arr = new Array(1000001).fill(0);
+const prefixSum = new Array(1000001).fill(0);
+
+function getSum(s, e) {
+    return prefixSum[e] - prefixSum[s - 1];
+}
+
+num.forEach(elem => arr[elem] = 1);
+
+prefixSum[0] = 0;
+for (let i = 1; i <= 1000000; i++)
+    prefixSum[i] = prefixSum[i - 1] + arr[i];
+
+range.forEach(([i, j]) => console.log(getSum(i, j)));
