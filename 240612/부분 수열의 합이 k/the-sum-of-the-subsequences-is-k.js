be2e40530e7a368267866f45941839f4b76613ca
Dong Kim <khdkkhdd@gmail.com>
2024-06-12 11:17:01 +0900

    Title: 부분 수열의 합이 k, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 240612/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.js
+++ 240612/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.js
@@ -5,24 +5,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].trim().split(' ').map(Number);
 const arr = [0].concat(input[1].trim().split(' ').map(Number));
 const prefixSum = new Array(n + 1).fill(0);
+let ans = 0;
 
+function getSum(s, e) {
+    return prefixSum[e] - prefixSum[s - 1];
+}
+
+// 누적합 배열을 만들어줍니다.
+prefixSum[0] = 0;
 for (let i = 1; i <= n; i++)
     prefixSum[i] = prefixSum[i - 1] + arr[i];
 
-let ans = 0;
-for (let len = 1; len <= n; len++) {
-    for (let s = 1; s <= n; s++) {
-        let e = s + len - 1;
-        if (e > n) continue;
-        if (prefixSum[e] - prefixSum[s - 1] === k) ans++;
+// 모든 구간에 대해 합을 찾아
+// 그 중 합이 k인 경우의 수를 세줍니다.
+for (let i = 1; i <= n; i++) {
+    for (let j = i; j <= n; j++) {
+        if (getSum(i, j) === k)
+            ans++;
     }
 }
-
-// for (let s = 1; s <= n; s++) {
-//     for (let e = s; e <= n; e++) {
-//         if (prefixSum[e] - prefixSum[s - 1] === k)
-//             ans++;
-//     }
-// }
 
 console.log(ans);
