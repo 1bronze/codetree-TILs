733de5d7f161989b4415e2ad822ce729530ee5d3
Dong Kim <khdkkhdd@gmail.com>
2024-06-13 15:54:31 +0900

    Title: 별이 있는 숫자로만의 이동, Time: 1331ms, Memory: 47MB, Status: Passed - CodeTree

--- 240613/별이 있는 숫자로만의 이동/move-to-numbers-with-star.js
+++ 240613/별이 있는 숫자로만의 이동/move-to-numbers-with-star.js
@@ -4,36 +4,30 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].trim().split(' ').map(Number);
 const arr = input.slice(1, 1 + n).map(line => line.trim().split(' ').map(Number));
 const prefixSum = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+let ans = 0;
 
-function inRange(target) {
-    return target > 0 && target <= n;
-}
-
-function getSum(x, s, e) {
-    return prefixSum[x][e] - prefixSum[x][s - 1];
-}
-
-function getManhattanSum(x, y) {
-    let sum = 0;
-    for (let i = x - k; i <= x + k; i++) {
-        let dy = k - Math.abs(x - i);
-        if (!inRange(i)) continue;
-
-        let s = inRange(y - dy) ? y - dy : 1;
-        let e = inRange(y + dy) ? y + dy : n;
-
-        sum += getSum(i, s, e);
-    }
-    return sum;
-}
-
+// 배열의 누적합을 구합니다.
 for (let i = 1; i <= n; i++)
     for (let j = 1; j <= n; j++)
         prefixSum[i][j] = prefixSum[i][j - 1] + arr[i - 1][j - 1];
 
-let ans = 0;
-for (let i = 1; i <= n; i++)
-    for (let j = 1; j <= n; j++)
-    ans = Math.max(ans, getManhattanSum(i, j));
+// 모든 중심에 대해 최댓값을 구합니다.
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        // 중심이 (i, j)일 때의 숫자 합을 구합니다.
+        let sumAll = 0;
+        for (let r = i - k; r <= i + k; r++) {
+            // r행일때 (j - c ~ j + c)열 까지의 부분합을 더해줍니다.
+            const c = k - Math.abs(i - r);
 
+            // r행이 범위 안에 있을 경우 부분합을 더해줍니다.
+            if (1 <= r && r <= n) {
+                sumAll += prefixSum[r][Math.min(j + c, n)] - prefixSum[r][Math.max(j - c - 1, 0)];
+            }
+        }
+        ans = Math.max(ans, sumAll);
+    }
+}
+
+// 정답을 출력합니다.
 console.log(ans);
