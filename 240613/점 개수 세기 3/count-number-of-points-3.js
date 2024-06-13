9032b6f75fc48d2416461e45ba3290ac2023da56
Dong Kim <khdkkhdd@gmail.com>
2024-06-13 18:57:18 +0900

    Title: 점 개수 세기 3, Time: 1596ms, Memory: 81MB, Status: Passed - CodeTree

--- 240613/점 개수 세기 3/count-number-of-points-3.js
+++ 240613/점 개수 세기 3/count-number-of-points-3.js
@@ -3,26 +3,28 @@ const SortedSet = require("collections/sorted-set");
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력:
 const [n, q] = input[0].trim().split(' ').map(Number);
-const nums = input[1].trim().split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
 const queries = input.slice(2, 2 + q).map(line => line.trim().split(' ').map(Number));
 
-const treeSet = new SortedSet();
+// 주어진 x좌표값들을 전부 treeset에 넣어줍니다.
+const nums = new SortedSet();
+arr.forEach(num => nums.push(num));
+
+// treeset에서 정점을 작은 번호부터 뽑으면서
+// 각 정점별로 1번부터 순서대로 매칭하여
+// 그 결과를 hashmap에 넣어줍니다.
 const mapper = new Map();
-
-nums.forEach(num => treeSet.push(num));
-
 let cnt = 1;
-treeSet.forEach(num => mapper.set(num, cnt++));
+nums.forEach(num => mapper.set(num, cnt++));
 
-queries.forEach(([s, e]) => {
-    let first = mapper.get(treeSet.findGreatestLessThanOrEqual(s).value);
-
-    let second;
-    if (treeSet.findLeastGreaterThan(e) !== undefined)
-        second = mapper.get(treeSet.findLeastGreaterThan(e).value);
-    else
-        second = n + 1;
-
-    console.log(second - first);
+// 질의에 대해
+// 각 [a, b]에 해당하는 번호를
+// mapper를 통해 구해
+// 두 번호 사이의 점의 수를 출력합니다.
+queries.forEach(([a, b]) => {
+    const newA = mapper.get(a);
+    const newB = mapper.get(b);
+    console.log(newB - newA + 1);
 });
